addcd487a9d838b6c60cc6b78ef72360a44f236c
Argo-Lenovo <Argo@163.com>
2016-10-21 11:03:13 +0800

    新建测试用例Demo工程，给开发人员使用

---
+++ Bootstrap.DataAccessTests/MenuTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Bootstrap.DataAccess.Tests
+{
+    [TestClass()]
+    public class MenuTests
+    {
+        [TestMethod()]
+        public void RetrieveMenusTest()
+        {
+            var result = TerminalHelper.RetrieveTerminals("1");
+            Assert.Equals(result.Count(), 1);
+            result = TerminalHelper.RetrieveTerminals("");
+            Assert.Equals(result.Count(), 2);
+        }
+    }
+}
