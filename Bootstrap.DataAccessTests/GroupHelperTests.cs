e5cba6bf5031cb9efb7adff3c17d74932bb562ea
liuchun_0206@163.com <liuchun_0206@163.com>
2016-10-22 15:48:38 +0800

    新增GroupHelperTest.cs测试功能

---
+++ Bootstrap.DataAccessTests/GroupHelperTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bootstrap.DataAccess.Tests
+{
+    [TestClass]
+    public class GroupHelperTests
+    {
+        [TestMethod]
+        public void RetrieveGroupsTest()
+        {
+            var result = GroupHelper.RetrieveGroups("1");
+            Assert.IsTrue((result.Count() == 0 || result.Count() == 1), "带有参数的GroupHelper.RetrieveGroups方法调用失败，请检查数据库连接或者数据库SQL语句");
+            result = GroupHelper.RetrieveGroups();
+            Assert.IsTrue(result.Count() >= 0, "不带参数的GroupHelper.RetrieveGroups方法调用失败，请检查数据库连接或者数据库SQL语句");
+        }
+
+        [TestMethod]
+        public void SaveGroupTest()
+        {
+            Group p = new Group();
+            p.GroupName = "测试群组2";
+            p.Description = "测试群组2";
+
+            var result = GroupHelper.SaveGroup(p);
+            Assert.IsTrue(result, "增加用户出错");
+
+            p.ID = 4;
+            p.GroupName = "测试群组22";
+            p.Description = "测试群组22";
+            result = GroupHelper.SaveGroup(p);
+            Assert.IsTrue(result, "更新用户出错");
+
+        }
+
+        [TestMethod]
+        public void DeleteGroupTest()
+        {
+            string p = "2";
+            try
+            {
+                GroupHelper.DeleteGroup(p);
+                Assert.IsTrue(true);
+            }
+            catch
+            {
+                Assert.IsTrue(false, "删除用户出错");
+            }
+        }
+
+
+    }
+}
