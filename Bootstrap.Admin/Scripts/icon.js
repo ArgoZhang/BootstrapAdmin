2f7bb03626ea3b927e81d7840bc08f96589be5f9
Argo-Lenovo <Argo@163.com>
2016-10-25 01:20:58 +0800

    增加FontAwesome与Glyphicon两种图标选择视图

---
+++ Bootstrap.Admin/Scripts/icon.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('.fontawesome-icon-list a').click(function () {
+        window.console.log($(this).children('i').attr('class'));
+        return false;
+    });
+
+    $('.fontawesome-icon-list ul li').click(function () {
+        window.console.log($(this).children('span:first').attr('class'));
+    });
+
+    $('.fontawesome-icon-list ul li').addClass('col-md-3 col-sm-4');
+});
