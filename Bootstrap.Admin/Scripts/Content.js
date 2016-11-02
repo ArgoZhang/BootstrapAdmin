ea5229cfbec2da5d886ae830ca616ec4bea93f48
Argo-Lenovo <Argo@163.com>
2016-11-02 12:12:53 +0800

    增加iframe自适应高度

--- Bootstrap.Admin/Scripts/Content.js
+++ Bootstrap.Admin/Scripts/Content.js
@@ -1,4 +1,12 @@
 ﻿$(function () {
-    var fra = $('iframe').get(0);
-    fra.height = fra.contentDocument.body.offsetHeight;
+    function iframeResposive() {
+        try {
+            var fra = $('iframe').get(0);
+            fra.height = fra.contentDocument.body.offsetHeight;
+        }
+        catch (e) {
+        }
+    }
+    $(window).on('load', iframeResposive);
+    $(window).on('resize', iframeResposive);
 });
