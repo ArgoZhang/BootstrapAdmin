a69d75e731cc17ef5f4561775815bfc156c8caaf
Argo-Lenovo <Argo@163.com>
2016-11-03 16:28:05 +0800

    增加父级菜单与菜单顺序弹窗功能

--- Bootstrap.Admin/Scripts/Menus.js
+++ Bootstrap.Admin/Scripts/Menus.js
@@ -95,18 +95,68 @@
         $('#btnIcon i').attr('class', icon);
     });
 
-    $('#btnMenu').click(function () {
+    // 排序按钮
+    $('#btnMenuOrder').click(function () {
+        $('li.dd-item').remove('[data-id="0"]');
         $('#dialogNew div.modal-dialog').hide();
+        $('.menu-content button:last').data('type', 'order');
+        // handler new menu
+        if ($('#menuID').val() == "") {
+            var menuName = $('#name').val();
+            if (menuName == "") menuName = "新建菜单-未命名";
+            $('.dd-list').append($.format('<li class="dd-item dd3-item" data-id="0"><div class="dd-handle dd3-handle"></div><div class="dd3-content"><label><span>{0}</span></label></div></li>', menuName));
+        }
+        var did = $('#menuID').val();
+        if (did == "") did = 0;
+        $('.dd-list li[data-id="' + did + '"] span').addClass('active');
         $('.menu-content').show();
+        $('div.dd3-content input').hide();
+    });
+
+    // 选择父节点按钮
+    $('#btnMenuParent').click(function () {
+        $('li.dd-item').remove('[data-id="0"]');
+        $('#dialogNew div.modal-dialog').hide();
+        $('.menu-content button:last').data('type', 'parent');
+        $('.menu-content').show();
+        $('div.dd3-content :checkbox').hide();
+        $('div.dd3-content :radio').show();
     });
 
     $('.menu-content button').click(function () {
+        // remove active css
+        $('.dd3-content span').removeClass('active');
         $('.menu-content').hide();
         $('#dialogNew div.modal-dialog').show();
     });
 
-    $('.menu-content button:last').click(function () {
+    //$('.dd3-content label').click(function (e) {
+    //    var c = $(this).parents('div').children('label').children('input:not(:hidden)');
+    //    if (c.length == 1) c.get(0).click();
+    //    window.console.log($(this).html());
+    //});
 
+    $('.menu-content button:last').click(function () {
+        var type = $('.menu-content button:last').data('type');
+        switch (type) {
+            case "parent":
+                $('#parentId').val($('.dd3-content :radio:checked').val());
+                $('#parentName').val($('.dd3-content :radio:checked').next('span').text());
+                break;
+            case "order":
+                var data = $('#nestable_menu').nestable('serialize');
+                var mid = $('#menuID').val();
+                for (index in data) {
+                    window.console.log(index);
+                    if (data[index].id == mid || data[index] == 0) {
+                        $('#order').val(10 + index * 10);
+                        break;
+                    }
+                }
+                break;
+            default:
+                break;
+        }
     });
 
     $('#nestable_menu').nestable();
