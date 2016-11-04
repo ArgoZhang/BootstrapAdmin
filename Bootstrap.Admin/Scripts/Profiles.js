dd67c1b3602339128a2465f5571fa96daae13b29
Argo-Lenovo <Argo@163.com>
2016-11-04 17:42:14 +0800

    调整缓存列表样式，缓存时间在Key右上角，支持删除指定缓存

--- Bootstrap.Admin/Scripts/Profiles.js
+++ Bootstrap.Admin/Scripts/Profiles.js
@@ -1,22 +1,32 @@
 ﻿$(function () {
-    var html = '<li class="list-primary"><i class="fa fa-ellipsis-v"></i><div class="task-title"><span class="task-title-sp">{2}</span><span class="badge badge-sm label-success">{0}</span><span class="task-value">{3}</span><div class="pull-right hidden-phone"><button class="btn btn-danger btn-xs fa fa-trash-o" data-val="{1}"></button></div></div></li>';
-    $.ajax({
-        url: '../../CacheList.axd',
-        type: 'GET',
-        success: function (result) {
-            if (result) {
-                result = $.parseJSON(result);
-                if ($.isArray(result)) {
-                    var content = result.map(function (ele) {
-                        return $.format(html, ele.Interval, ele.Key, ele.Desc, ele.Value);
-                    }).join('');
-                    $('#sortable').append(content);
+    var html = '<li class="list-primary"><i class="fa fa-ellipsis-v"></i><div class="task-title"><span class="task-title-sp">{2}</span><span class="badge badge-sm label-success">{0}</span><span class="task-value tooltips" data-placement="top" data-original-title="{3}">{3}</span><div class="pull-right hidden-phone"><button class="btn btn-danger btn-xs fa fa-trash-o tooltips" data-key="{1}" data-placement="left" data-original-title="{1}"></button></div></div></li>';
+
+    function listCache(options) {
+        $.ajax({
+            url: options.url,
+            type: 'GET',
+            success: function (result) {
+                if (result) {
+                    result = $.parseJSON(result);
+                    if ($.isArray(result)) {
+                        var content = result.map(function (ele) {
+                            return $.format(html, ele.Interval, ele.Key, ele.Desc, ele.Value);
+                        }).join('');
+                        $('#sortable').html(content);
+                        $('.tooltips').tooltip();
+                        $('#sortable .btn').click(function () {
+                            var key = $(this).attr('data-key');
+                            listCache({ url: $.format('../../CacheList.axd?cacheKey={0}', key) });
+                        });
+                    }
                 }
+                else {
+                }
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
             }
-            else {
-            }
-        },
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-        }
-    });
+        });
+    }
+
+    listCache({ url: '../../CacheList.axd' });
 });
