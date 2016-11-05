e7ce6449be2a7330327226afc7c1b691527209cb
Argo-Lenovo <Argo@163.com>
2016-11-06 01:24:05 +0800

    后台增加welcome页面

---
+++ Bootstrap.Admin/Scripts/well.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    $('#main-content').css('backgroundImage', 'url("../../content/images/bg.jpg")');
+    function resposive() {
+        $('.well-bg').height($(window).height() - 270);
+    }
+    $(window).on('load', resposive);
+    $(window).on('resize', resposive);
+});
