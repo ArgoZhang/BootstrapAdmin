5f4e12c7fa9bef43b4c93dd007c0565446ddfbf2
Argo-Lenovo <Argo@163.com>
2016-11-09 16:48:44 +0800

    增加新功能，屏幕宽度太小时，toolbar变成图标

--- Bootstrap.Admin/Content/js/common-scripts.js
+++ Bootstrap.Admin/Content/js/common-scripts.js
@@ -71,4 +71,12 @@ $(function () {
     //$("html").niceScroll({styler:"fb",cursorcolor:"#e8403f", cursorwidth: '6', cursorborderradius: '10px', background: '#404040', spacebarenabled:false,  cursorborder: '', zindex: '1000', scrollspeed: 100, mousescrollstep: 60});
 
     //$(".table-responsive").niceScroll({ styler: "fb", cursorcolor: "#e8403f", cursorwidth: '6', cursorborderradius: '10px', background: '#404040', spacebarenabled: false, cursorborder: '', zindex: '1000', horizrailenabled: true });
+
+
+    $('.toolbar').insertBefore($('.bootstrap-table > .fixed-table-toolbar > .bs-bars'));
+
+    $('.toolbar a').click(function () {
+        var ctl = $('#' + $(this).attr('id').replace('tb_', 'btn_'));
+        ctl.trigger("click");
+    });
 })(jQuery);
