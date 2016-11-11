f9ff09b345fe7a1a64c33b99c332efe730c64db9
Argo-Lenovo <Argo@163.com>
2016-11-11 13:23:01 +0800

    更改为UTF-8编码

--- Bootstrap.Admin/Content/js/messages_zh.js
+++ Bootstrap.Admin/Content/js/messages_zh.js
@@ -1,4 +1,4 @@
-(function (factory) {
+﻿(function (factory) {
     if (typeof define === "function" && define.amd) {
         define(["jquery", "../jquery.validate"], factory);
     } else if (typeof module === "object" && module.exports) {
