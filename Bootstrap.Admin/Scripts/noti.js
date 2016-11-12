894921fc7895cbdbdb3a8dbfe78a144028c9981e
Argo-Lenovo <Argo@163.com>
2016-11-12 19:53:25 +0800

    更改app/db按钮图标与颜色

--- Bootstrap.Admin/Scripts/noti.js
+++ Bootstrap.Admin/Scripts/noti.js
@@ -2,8 +2,8 @@
     var url = '../api/Notifications/';
 
     var htmlNewUsers = '<li class="list-primary"><i class="fa fa-ellipsis-v"></i><div class="task-title notifi"><span class="task-title-sp">{0}</span><span class="task-value">{1}</span><span class="task-time">{2}</span><div class="pull-right hidden-phone"><button class="btn btn-success btn-xs fa fa-check" data-id="{3}" data-result="1"></button><button class="btn btn-danger btn-xs fa fa-remove" data-id="{3}" data-result="0" data-placement="left" data-original-title="拒绝授权"></button></div></div></li>';
-    var htmlApp = '<li class="list-warning"><i class="fa fa-ellipsis-v"></i><div class="task-title notifi"><span class="task-title-sp">{0}</span><span class="task-value">{1}</span><span class="task-time">{2}</span><div class="pull-right hidden-phone"><a href="../Admin/Exceptions/{3}" class="btn btn-success btn-xs fa fa-reorder"></a></div></div></li>';
-    var htmlDb = '<li class="list-danger"><i class="fa fa-ellipsis-v"></i><div class="task-title notifi"><span class="task-title-sp">{0}</span><span class="task-value">{1}</span><span class="task-time">{2}</span><div class="pull-right hidden-phone"><a href="../Admin/Exceptions/{3}" class="btn btn-success btn-xs fa fa-reorder"></a></div></div></li>';
+    var htmlApp = '<li class="list-warning"><i class="fa fa-ellipsis-v"></i><div class="task-title notifi"><span class="task-title-sp">{0}</span><span class="task-value">{1}</span><span class="task-time">{2}</span><div class="pull-right hidden-phone"><a href="../Admin/Exceptions/{3}" class="btn btn-warning btn-xs fa fa-bug"></a></div></div></li>';
+    var htmlDb = '<li class="list-danger"><i class="fa fa-ellipsis-v"></i><div class="task-title notifi"><span class="task-title-sp">{0}</span><span class="task-value">{1}</span><span class="task-time">{2}</span><div class="pull-right hidden-phone"><a href="../Admin/Exceptions/{3}" class="btn btn-danger btn-xs fa fa-database"></a></div></div></li>';
 
     function listData(options) {
         options = $.extend({ url: url, animation: true, ctl: $('a.fa-refresh') }, options);
