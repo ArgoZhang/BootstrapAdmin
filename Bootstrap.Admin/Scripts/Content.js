9f01d7a88a1ed4ca29f57cd5f5c5d4043b52fcbf
Argo-Lenovo <Argo@163.com>
2016-11-19 13:07:34 +0800

    修复BUG：前台页面跳转时菜单没有正确设置active样式

--- Bootstrap.Admin/Scripts/Content.js
+++ Bootstrap.Admin/Scripts/Content.js
@@ -1,6 +1,5 @@
 ﻿$(function () {
     $('#navbar').attr('data-toggle', "dropdown").addClass('dropdown-toggle');
-
     function iframeResposive() {
         try {
             var fra = $('iframe').get(0);
@@ -12,13 +11,22 @@
     $(window).on('load', iframeResposive);
     $(window).on('resize', iframeResposive);
 
-    $('.menu-submenu a').click(function (event) {
-        var act = $(this).attr("data-act");
+    var $subMenu = $('#submenu');
+    $subMenu.on('click', 'a', function (event) {
+        var $this = $(this);
+        var act = $this.attr("data-act");
         if (act === "True") return true;
         event.preventDefault();
-        $('.menu-submenu a, .menu-submenu p').removeClass('active');
-        $(this).addClass('active');
-        $(this).parents("ul").first().find('p').addClass('active');
-        $('iframe').attr('src', $(this).attr('href'));
+        $subMenu.find('p, a').removeClass('active');
+        setActive($this);
+        $('iframe').attr('src', $this.attr('href'));
     });
+    $subMenu.find('a').each(function (index, ele) {
+        var $this = $(this);
+        if ($this.attr('href') == window.location.pathname) setActive($this);
+    });
+    function setActive(ele) {
+        ele.addClass('active');
+        ele.parents("ul").first().find('p').addClass('active');
+    }
 });
