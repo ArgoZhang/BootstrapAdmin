a06380fcaf1609451f4a6a49b8519a6b5d881db3
Argo-Lenovo <Argo@163.com>
2016-11-23 13:37:20 +0800

    修改BUG：图片路径不对

--- Bootstrap.Admin/Scripts/well.js
+++ Bootstrap.Admin/Scripts/well.js
@@ -1,5 +1,5 @@
 ﻿$(function () {
-    $('#main-content').css('backgroundImage', 'url("../../content/images/bg.jpg")');
+    $('#main-content').css('backgroundImage', 'url("../content/images/bg.jpg")');
     function resposive() {
         var height = $(window).height();
         if (height > 672)
