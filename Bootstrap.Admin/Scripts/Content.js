773270948b2911f82db3d632d1bb23a154e44ade
Argo-Lenovo <Argo@163.com>
2016-11-25 17:28:04 +0800

    移除链接增加时间戳

--- Bootstrap.Admin/Scripts/Content.js
+++ Bootstrap.Admin/Scripts/Content.js
@@ -3,13 +3,4 @@
     var $subMenu = $('#submenu');
     var $breadNav = $('#breadNav');
     $breadNav.hide();
-    $subMenu.on('click', 'a', function () {
-        var $this = $(this);
-        var href = $this.attr('href');
-        var tail = new Date().format("HHmmss");
-        if (href.indexOf('?') > -1) href += '&ba=' + tail;
-        else href += '?ba=' + tail;
-        window.location.href = href;
-        return false;
-    });
 });
