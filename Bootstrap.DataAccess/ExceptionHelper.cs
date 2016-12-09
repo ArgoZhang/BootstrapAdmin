8bc89a190738f17c04c8ff7cc62d8d63d6a667e8
Argo-Lenovo <Argo@163.com>
2016-12-09 13:01:27 +0800

    防止数据过大选取Top 1000数据

--- Bootstrap.DataAccess/ExceptionHelper.cs
+++ Bootstrap.DataAccess/ExceptionHelper.cs
@@ -47,7 +47,7 @@ namespace Bootstrap.DataAccess
         {
             return CacheManager.GetOrAdd(RetrieveExceptionsDataKey, CacheSection.RetrieveIntervalByKey(RetrieveExceptionsDataKey), key =>
             {
-                string sql = "select * from Exceptions";
+                string sql = "select top 1000 * from Exceptions";
                 List<Exceptions> Exceptions = new List<Exceptions>();
                 DbCommand cmd = DBAccessManager.SqlDBAccess.CreateCommand(CommandType.Text, sql);
                 try
