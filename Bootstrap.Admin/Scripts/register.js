04663aa6f757cca1265f0d2d318ba38dcae75289
Argo-Lenovo <Argo@163.com>
2016-12-15 16:25:01 +0800

    重新用户注册页面，适配小屏幕

--- Bootstrap.Admin/Scripts/register.js
+++ Bootstrap.Admin/Scripts/register.js
@@ -15,23 +15,29 @@
         assurePassword: {
             required: true,
             maxlength: 50,
-            equalTo:"#password"
+            equalTo: "#password"
         },
         description: {
             required: true,
             maxlength: 500
-    }
+        }
     });
 
     $('#btnAccount').click(function () {
         var valid = $('form').valid();
         if (valid) {
-                $('.setup-main').hide();
-                $('.steps li').toggleClass('current');
-                $('#loginID').text($('#userName').val());
-                $('#loginName').text($('#displayName').val());
-                $('#loginDesc').text($('#description').val());
-                $('.setup-confirm').show();            
+            $('.setup-main').hide();
+            $('.steps li').toggleClass('current');
+            $('#loginID').text($('#userName').val());
+            $('#loginName').text($('#displayName').val());
+            $('#loginDesc').text($('#description').val());
+            $('.setup-confirm').show();
         }
+    });
+
+    $('#btnPrev').click(function () {
+        $('.steps li').toggleClass('current');
+        $('.setup-main').show();
+        $('.setup-confirm').hide();
     });
 });
