c12af231ecf63f3c9d86afb621fdc5ab7a16f30f
Argo-Lenovo <Argo@163.com>
2017-03-14 23:11:05 +0800

    修改BUG：更改脚本中的样式表路径

--- Bootstrap.Admin/Scripts/Longbow.Common.js
+++ Bootstrap.Admin/Scripts/Longbow.Common.js
@@ -352,7 +352,7 @@ $(function () {
         Id: 1, url: Dicts.url, data: { type: 'activeCss' }, swal: false,
         callback: function (result) {
             if (result.length > 0)
-                $('head').append($.format('<link href="../Content/css/{0}" rel="stylesheet" type="text/css" />', result[0].Code));
+                $('head').append($.format('<link href="../Content/{0}" rel="stylesheet" type="text/css" />', result[0].Code));
         }
     });
 });
