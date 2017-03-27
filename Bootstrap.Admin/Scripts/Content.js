150473af3e12de87655da3ae92c722a051cdf31f
Argo-Lenovo <Argo@163.com>
2017-03-27 14:59:42 +0800

    重构代码：精简前台后台首页代码，移除js文件，精简样式代码

--- Bootstrap.Admin/Scripts/Content.js
+++ Bootstrap.Admin/Scripts/Content.js
@@ -1,6 +1,4 @@
 ﻿$(function () {
     $('#navbar').attr('data-toggle', "dropdown").addClass('dropdown-toggle');
-    var $subMenu = $('#submenu');
-    var $breadNav = $('#breadNav');
-    $breadNav.hide();
+    $('#breadNav').hide();
 });
