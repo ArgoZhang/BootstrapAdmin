d3ad8e768d9addfc475cb814c32015ee99b004c4
Argo-MacBookPro <Argo@163.com>
2018-08-01 21:54:54 +0800

    更新环境：升级组件版本到bootstrapV4.1.3

--- Bootstrap.Admin/wwwroot/js/Login.js
+++ Bootstrap.Admin/wwwroot/js/Login.js
@@ -1,15 +1,3 @@
 ﻿$(function () {
     $(".container").autoCenter();
-
-    // validate
-    $('#login').autoValidate({
-        userName: {
-            required: true,
-            maxlength: 50
-        },
-        password: {
-            required: true,
-            maxlength: 50
-        }
-    });
 })
