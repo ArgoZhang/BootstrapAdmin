0fc128d3d8006e98ef07e209bb3b0d2573a9683a
Argo-MacBookPro <Argo@163.com>
2018-08-26 15:44:19 +0800

    重构代码：api/New请求地址物理文件更改到api目录下

---
+++ Bootstrap.Admin/Controllers/Api/NewController.cs
@@ -0,0 +1,27 @@
+﻿using Bootstrap.DataAccess;
+using Bootstrap.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Bootstrap.Admin.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("api/[controller]")]
+    public class NewController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AllowAnonymous]
+        public bool Get(string userName)
+        {
+            return BootstrapUser.RetrieveUserByUserName(userName) == null && !UserHelper.RetrieveNewUsers().Any(u => u.UserName == userName);
+        }
+    }
+}
