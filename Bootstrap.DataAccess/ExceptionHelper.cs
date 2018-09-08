e556556e7cea3746aad3bf61f680f8b5ba2f8f0e
Argo-MacBookPro <Argo@163.com>
2018-09-08 19:37:42 +0800

    增加功能：异常记录数据库增加自动清理功能，增加配置项KeepExceptionsPeriod，生产环境保存12个月，开发环境保存1个月，界面显示一周内异常数据

--- Bootstrap.DataAccess/ExceptionHelper.cs
+++ Bootstrap.DataAccess/ExceptionHelper.cs
@@ -1,6 +1,6 @@
 ﻿using Longbow.Cache;
+using Longbow.Configuration;
 using Longbow.Data;
-using Longbow.Logging;
 using Longbow.Web.WebSockets;
 using Newtonsoft.Json;
 using System;
@@ -45,41 +45,50 @@ namespace Bootstrap.DataAccess
                 var category = "App";
                 if (ex.GetType().IsSubclassOf(typeof(DbException))) category = "DB";
                 WebSocketServerManager.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(new MessageBody[] { new MessageBody() { Category = category, Message = ex.Message } }))));
+                ClearExceptionsAsync();
             }
         }
         /// <summary>
-        /// 查询所有异常
+        /// 
+        /// </summary>
+        private static void ClearExceptionsAsync()
+        {
+            System.Threading.Tasks.Task.Run(() =>
+            {
+                string sql = $"delete from Exceptions where LogTime < DATEADD(MONTH, -{ConfigurationManager.AppSettings["KeepExceptionsPeriod"]}, GETDATE())";
+                DbCommand cmd = DBAccessManager.SqlDBAccess.CreateCommand(CommandType.Text, sql);
+                DBAccessManager.SqlDBAccess.ExecuteNonQuery(cmd);
+            });
+        }
+        /// <summary>
+        /// 查询一周内所有异常
         /// </summary>
         /// <returns></returns>
         public static IEnumerable<Exceptions> RetrieveExceptions()
         {
             return CacheManager.GetOrAdd(RetrieveExceptionsDataKey, key =>
             {
-                string sql = "select top 1000 * from Exceptions order by LogTime desc";
+                string sql = "select * from Exceptions where DATEDIFF(Week, LogTime, GETDATE()) = 0 order by LogTime desc";
                 List<Exceptions> exceptions = new List<Exceptions>();
                 DbCommand cmd = DBAccessManager.SqlDBAccess.CreateCommand(CommandType.Text, sql);
-                try
+                using (DbDataReader reader = DBAccessManager.SqlDBAccess.ExecuteReader(cmd))
                 {
-                    using (DbDataReader reader = DBAccessManager.SqlDBAccess.ExecuteReader(cmd))
+                    while (reader.Read())
                     {
-                        while (reader.Read())
+                        exceptions.Add(new Exceptions()
                         {
-                            exceptions.Add(new Exceptions()
-                            {
-                                Id = (int)reader[0],
-                                AppDomainName = (string)reader[1],
-                                ErrorPage = reader.IsDBNull(2) ? string.Empty : (string)reader[2],
-                                UserId = reader.IsDBNull(3) ? string.Empty : (string)reader[3],
-                                UserIp = reader.IsDBNull(4) ? string.Empty : (string)reader[4],
-                                ExceptionType = (string)reader[5],
-                                Message = (string)reader[6],
-                                StackTrace = (string)reader[7],
-                                LogTime = (DateTime)reader[8],
-                            });
-                        }
+                            Id = (int)reader[0],
+                            AppDomainName = (string)reader[1],
+                            ErrorPage = reader.IsDBNull(2) ? string.Empty : (string)reader[2],
+                            UserId = reader.IsDBNull(3) ? string.Empty : (string)reader[3],
+                            UserIp = reader.IsDBNull(4) ? string.Empty : (string)reader[4],
+                            ExceptionType = (string)reader[5],
+                            Message = (string)reader[6],
+                            StackTrace = (string)reader[7],
+                            LogTime = (DateTime)reader[8],
+                        });
                     }
                 }
-                catch (Exception ex) { ExceptionManager.Publish(ex); }
                 return exceptions;
             });
         }
