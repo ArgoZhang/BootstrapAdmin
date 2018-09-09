1d8b3ea9a74151b9e6efb1c8baa6abd823aab34a
Argo-MacBookPro <Argo@163.com>
2018-09-09 14:16:03 +0800

    更新脚本：增加系统日志默认按照记录时间倒叙排序

--- Bootstrap.Admin/wwwroot/js/logs.js
+++ Bootstrap.Admin/wwwroot/js/logs.js
@@ -4,6 +4,7 @@
     $('table').smartTable({
         url: url,
         sortName: 'LogTime',
+        sortOrder: 'desc',
         queryParams: function (params) { return $.extend(params, { operateType: $("#txt_operate_type").val(), OperateTimeStart: $("#txt_operate_start").val(), OperateTimeEnd: $("#txt_operate_end").val() }); },
         columns: [
             { title: "操作类型", field: "CRUD", sortable: true },
