f83717e72415ec2ddb5c3468fbac83e35adba6c3
Argo-MacBookPro <Argo@163.com>
2018-09-12 22:29:45 +0800

    重构代码：FAIcon页面使用ajax调用图标局域视图，利用已经被缓存机制减少网络带宽

--- Bootstrap.Admin/wwwroot/js/icon.js
+++ Bootstrap.Admin/wwwroot/js/icon.js
@@ -1,11 +1,21 @@
 ﻿$(function () {
-    var $iconList = $('div.fontawesome-icon-list').on('click', 'a', function () {
-        window.console.log($(this).children('i').attr('class'));
-        return false;
+    $.bc({
+        url: Menu.iconView,
+        contentType: 'text/html',
+        dataType: 'html',
+        callback: function (result) {
+            if (result) {
+                $('#main-content').html(result);
+                var $iconList = $('div.fontawesome-icon-list').on('click', 'a', function () {
+                    window.console.log($(this).children('i').attr('class'));
+                    return false;
+                });
+
+                $iconList.find('ul li').addClass('col-xl-2 col-md-3 col-sm-4 col-6');
+                $iconList.find('div').addClass('col-xl-2 col-6');
+
+                $('#main-content').scrollspy({ offset: 150, target: '.fa-nav' });
+            }
+        }
     });
-
-    $iconList.find('ul li').addClass('col-xl-2 col-md-3 col-sm-4 col-6');
-    $iconList.find('div').addClass('col-xl-2 col-6');
-
-    $('#main-content').scrollspy({ offset: 150, target: '.fa-nav' });
 });
