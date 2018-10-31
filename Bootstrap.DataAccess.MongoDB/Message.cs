2e273ef13e7588591a3d25da40e4cd240f107810
Argo-MacBookPro <Argo@163.com>
2018-10-31 17:50:13 +0800

    增加功能：完善Message类方法

--- Bootstrap.DataAccess.MongoDB/Message.cs
+++ Bootstrap.DataAccess.MongoDB/Message.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 
 namespace Bootstrap.DataAccess.MongoDB
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Message : DataAccess.Message
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns></returns>
         protected override IEnumerable<DataAccess.Message> RetrieveMessages(string userName)
         {
-            //TODO: 完善其他字段信息
             var msg = MongoDbAccessManager.DBAccess.GetCollection<DataAccess.Message>("Messages");
             return msg.Find(message => message.To == userName || message.From == userName).ToList();
         }
