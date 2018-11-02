64f02c469af8a1ba3f991f32ae816061d1fa5e18
Argo-MacBookPro <Argo@163.com>
2018-11-02 17:35:42 +0800

    增加工程：增加单元测试

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Bootstrap.Admin;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var server = new TestServer(WebHost.CreateDefaultBuilder()
+            .UseStartup<Startup>());
+
+        }
+    }
+}
