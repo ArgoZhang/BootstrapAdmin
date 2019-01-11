5e188ebfcff04a4c4a49429ec4d0c2f845bfb7be
Argo-Surface <Argo@163.com>
2019-01-12 00:34:39 +0800

    单元测试：测试文件增加多数据库配置项

--- UniTest/Bootstrap.DataAccess/BootstrapAdminStartup.cs
+++ UniTest/Bootstrap.DataAccess/BootstrapAdminStartup.cs
@@ -8,9 +8,19 @@ namespace Bootstrap.DataAccess
     {
         public BootstrapAdminStartup()
         {
+            var sqlConnectionStrings = "Data Source=.;Initial Catalog=UnitTest;User ID=sa;Password=sa";
+            var mysqlConnectionStrings = "Server=10.211.55.2;Database=BA;Uid=argozhang;Pwd=argo@163.com;SslMode=none;";
             var config = new ConfigurationBuilder().AddInMemoryCollection(new KeyValuePair<string, string>[] {
-                new KeyValuePair<string, string>("ConnectionStrings:ba", "Data Source=.;Initial Catalog=UnitTest;User ID=sa;Password=sa"),
+                new KeyValuePair<string, string>("ConnectionStrings:ba", sqlConnectionStrings),
                 new KeyValuePair<string, string>("DB:0:Enabled", "false"),
+
+                new KeyValuePair<string, string>("DB:1:Enabled", "false"),
+                new KeyValuePair<string, string>("DB:1:ProviderName", "SQLite"),
+                new KeyValuePair<string, string>("DB:1:ConnectionStrings:ba", "Data Source=UnitTest.db"),
+
+                new KeyValuePair<string, string>("DB:2:Enabled", "false"),
+                new KeyValuePair<string, string>("DB:2:ProviderName", "MySql"),
+                new KeyValuePair<string, string>("DB:2:ConnectionStrings:ba", mysqlConnectionStrings),
                 new KeyValuePair<string, string>("LongbowCache:Enabled", "false")
             }).Build();
             var sc = new ServiceCollection();
