6faa64fb738d46718da9b5a5d474756a5b990289
Argo-Surface <Argo@163.com>
2019-01-16 17:17:06 +0800

    单元测试：增加TasksController单元测试

---
+++ UnitTest/Bootstrap.Admin/Api/TasksTest.cs
@@ -0,0 +1,21 @@
+﻿using Bootstrap.DataAccess;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Bootstrap.Admin.Api
+{
+    public class TasksTest : ApiTest
+    {
+        public TasksTest(BAWebHost factory) : base(factory, "Tasks", true)
+        {
+
+        }
+
+        [Fact]
+        public async void Get_Ok()
+        {
+            var resp = await Client.GetAsJsonAsync<IEnumerable<Task>>();
+            Assert.NotNull(resp);
+        }
+    }
+}
