53bfd3af081a38bb9087cf147a7e28f9f2cebad6
Argo-Surface <Argo@163.com>
2019-01-16 14:41:17 +0800

    单元测试：增加MessagesController单元测试

---
+++ UnitTest/Bootstrap.Admin/Api/MessagesTest.cs
@@ -0,0 +1,33 @@
+﻿using Bootstrap.Admin.Models;
+using Bootstrap.DataAccess;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Bootstrap.Admin.Api
+{
+    public class MessagesTest : ApiTest
+    {
+        public MessagesTest(BAWebHost factory) : base(factory, "Messages", true)
+        {
+
+        }
+
+        [Theory]
+        [InlineData("inbox")]
+        [InlineData("sendmail")]
+        [InlineData("mark")]
+        [InlineData("trash")]
+        public async void Get_Ok(string action)
+        {
+            var resp = await Client.GetAsJsonAsync<IEnumerable<Message>>(action);
+            Assert.NotNull(resp);
+        }
+
+        [Fact]
+        public async void GetCount_Ok()
+        {
+            var resp = await Client.GetAsJsonAsync<MessageCountModel>(string.Empty);
+            Assert.NotNull(resp);
+        }
+    }
+}
