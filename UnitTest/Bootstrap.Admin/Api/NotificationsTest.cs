ada2ebdd388e8b46022c84380773a1e5ca1ede97
Argo-Surface <Argo@163.com>
2019-01-16 15:40:59 +0800

    单元测试：增加NotificationsController单元测试

---
+++ UnitTest/Bootstrap.Admin/Api/NotificationsTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Bootstrap.Admin.Api
+{
+    public class NotificationsTest : ApiTest
+    {
+        public NotificationsTest(BAWebHost factory) : base(factory, "Notifications", true)
+        {
+
+        }
+
+        [Fact]
+        public async void Get_Ok()
+        {
+            var resp = await Client.GetAsJsonAsync<object>();
+            Assert.NotNull(resp);
+        }
+    }
+}
