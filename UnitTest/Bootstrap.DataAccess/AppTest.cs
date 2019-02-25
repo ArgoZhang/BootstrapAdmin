45d2394f155f1cc9f3c18806afe9813c3cd1bf52
Argo-Surface <Argo@163.com>
2019-02-25 15:14:27 +0800

    单元测试：增加App类单元测试

---
+++ UnitTest/Bootstrap.DataAccess/AppTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Bootstrap.DataAccess
+{
+    [Collection("SQLServerContext")]
+    public class AppTest
+    {
+        [Fact]
+        public void RetrievesByRoleId_Ok()
+        {
+            var db = DbManager.Create();
+            db.Execute("delete from RoleApp");
+            var rid = new Role().Retrieves().Where(r => r.RoleName == "Administrators").First().Id;
+            var app = new App();
+            Assert.NotEmpty(app.RetrievesByRoleId(rid));
+        }
+
+        [Fact]
+        public void RetrievesByUserName_Ok()
+        {
+            var app = new App();
+            Assert.NotEmpty(app.RetrievesByUserName("Admin"));
+        }
+
+        [Fact]
+        public void SaveByRoleId_Ok()
+        {
+            var db = DbManager.Create();
+            db.Execute("delete from RoleApp");
+
+            var rid = new Role().Retrieves().Where(r => r.RoleName == "Administrators").First().Id;
+            var app = new App();
+            Assert.True(app.SaveByRoleId(rid, new string[] { "1", "2" }));
+
+            var count = db.ExecuteScalar<int>("select count(Id) from RoleApp where RoleID = @0", rid);
+            Assert.Equal(2, count);
+        }
+    }
+}
