78a38b7f8bb705cb83578100568c40bd6e4d036c
Argo-MacBookPro <Argo@163.com>
2019-03-27 14:09:42 +0800

    增加功能：系统界面演示系统相关提示信息 link #ITEI0

--- Bootstrap.Admin/Models/ModelBase.cs
+++ Bootstrap.Admin/Models/ModelBase.cs
@@ -15,18 +15,27 @@ namespace Bootstrap.Admin.Models
             Title = DictHelper.RetrieveWebTitle();
             Footer = DictHelper.RetrieveWebFooter();
             Theme = DictHelper.RetrieveActiveTheme();
+            IsDemo = DictHelper.RetrieveSystemModel();
         }
+
         /// <summary>
         /// 
         /// </summary>
         public string Title { get; private set; }
+
         /// <summary>
         /// 
         /// </summary>
         public string Footer { get; private set; }
+
         /// <summary>
         /// 
         /// </summary>
         public string Theme { get; protected set; }
+
+        /// <summary>
+        /// 是否为演示系统
+        /// </summary>
+        public bool IsDemo { get; protected set; }
     }
 }
