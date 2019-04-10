32894af881133a4762db578962688fff6171d0bb
Argo Zhang <Argo@163.com>
2019-04-10 13:22:15 +0800

    增加功能更：前台演示界面增加行为验证码示例

---
+++ Bootstrap.Client/wwwroot/js/index.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    $('#captcha').sliderCaptcha({
+        setSrc: function () {
+            return 'http://pocoafrro.bkt.clouddn.com/Pic' + Math.round(Math.random() * 136) + '.jpg';
+        },
+        onSuccess: function () {
+            var that = this;
+            setTimeout(() => {
+                that.parent().removeClass('d-inline-block');
+                that.sliderCaptcha('reset');
+            }, 1000);
+        }
+    });
+
+    $('#btnCaptcha').on('click', function () {
+        $('#captcha').parent().addClass('d-inline-block');
+    });
+
+    $.footer();
+});
