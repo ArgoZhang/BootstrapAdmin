e37836f1b48280bbe51c4590338083d7ae9bbf2b
Argo Zhang <Argo@163.com>
2019-04-18 15:37:26 +0800

    增加功能：默认首页母版页更改为_Frame

--- Bootstrap.Admin/Controllers/HomeController.cs
+++ Bootstrap.Admin/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Bootstrap.Admin.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
-            var model = new HeaderBarModel(User.Identity);
+            var model = new NavigatorBarModel(this);
             var url = DictHelper.RetrieveHomeUrl(model.AppCode);
             return url.Equals("~/Home/Index", System.StringComparison.OrdinalIgnoreCase) ? (IActionResult)View(model) : Redirect(url);
         }
