9959d7cfbc6dbf17a8aaac62f3b1e9f2acf63b39
Argo Zhang <Argo@163.com>
2019-06-02 00:18:21 +0800

    doc(test): 重构Appveyor单元测试命令

--- appveyor.test.ps1
+++ appveyor.test.ps1
@@ -1,4 +1,4 @@
-if ("$($env:APPVEYOR_REPO_BRANCH)" -eq "master") {
+function installDB() {
     write-host "init sqlserver database..." -ForegroundColor Cyan
     $startPath = "$($env:appveyor_build_folder)\DatabaseScripts\SqlServer"
     $sqlInstance = "(local)\SQL2014"
@@ -34,14 +34,36 @@ if ("$($env:APPVEYOR_REPO_BRANCH)" -eq "master") {
     iex "& $cmd"
 
     cd $($env:appveyor_build_folder)
+}
+
+function coverallUnitTest() {
+    write-host "install coveralls.net tools" -ForegroundColor Cyan
+    dotnet tool install coveralls.net --version 1.0.0 --tool-path "./tools"    
+
+    write-host "dotnet test UnitTest with Coveralls" -ForegroundColor Cyan
+    dotnet test UnitTest --no-restore /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Include="[Bootstrap*]*" /p:ExcludeByFile="../Bootstrap.Admin/Program.cs%2c../Bootstrap.Admin/Startup.cs%2c../Bootstrap.Admin/HttpHeaderOperation.cs" /p:CoverletOutput=../
+    cmd.exe /c ".\tools\csmacnz.Coveralls.exe --opencover -i coverage.opencover.xml --useRelativePaths"
+}
+
+if ("$($env:APPVEYOR_REPO_BRANCH)" -eq "master") {
+    installDB
 
     if ("$($env:APPVEYOR_REPO_PROVIDER)" -eq "gitHub") {
-        write-host "install coveralls.net tools" -ForegroundColor Cyan
-        dotnet tool install coveralls.net --version 1.0.0 --tool-path "./tools"
+        coverallUnitTest
+    }
+    elseif ("$($env:APPVEYOR_REPO_PROVIDER)" -eq "gitLab") {
+        coverallUnitTest
 
-        write-host "dotnet test UnitTest with Coveralls" -ForegroundColor Cyan
-        dotnet test UnitTest --no-restore /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Include="[Bootstrap*]*" /p:ExcludeByFile="../Bootstrap.Admin/Program.cs%2c../Bootstrap.Admin/Startup.cs%2c../Bootstrap.Admin/HttpHeaderOperation.cs" /p:CoverletOutput=../
-        cmd.exe /c ".\tools\csmacnz.Coveralls.exe --opencover -i coverage.opencover.xml --useRelativePaths"
+        Set-AppveyorBuildVariable COVERALLS_REPO_TOKEN $($env:COVERALLS_REPO_TOKEN_GITLAB)
+        Set-AppveyorBuildVariable CODECOV_TOKEN $($env:CODECOV_TOKEN_GITLAB)
+
+        $codecovCmd = "C:\ProgramData\chocolatey\lib\codecov\tools\codecov.exe"
+        $codecov = Test-Path $codecovCmd
+        if (!$codecov) {
+            write-host "install codecov tools" -ForegroundColor Cyan
+            choco install codecov
+        }
+        cmd.exe /c "$codecovCmd -f ""coverage.opencover.xml"""
     }
     else {
         write-warning "Coveralls has been skipped because current provider is ""$($env:APPVEYOR_REPO_PROVIDER)"""
