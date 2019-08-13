088ff671aeb443352c85b01e58235acdd1a8cc16
Argo Zhang <argo@163.com>
2019-08-13 17:31:30 +0800

    feat: 增加后台管理健康检查页面

---
+++ Bootstrap.Admin/wwwroot/js/healths.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    var $table = $('table').smartTable({
+        sidePagination: "client",
+        showToggle: false,
+        showRefresh: false,
+        showColumns: false,
+        columns: [
+            { title: "分类", field: "name", sortable: true },
+            { title: "描述", field: "Description", sortable: true },
+            { title: "异常信息", field: "Exception", sortable: true },
+            { title: "明细数据", field: "Data", sortable: true },
+            { title: "检查结果", field: "Status", sortable: false },
+            { title: "耗时", field: "Duration", sortable: true }
+        ]
+    });
+
+    $.bc({
+        url: 'healths',
+        callback: function (result) {
+            console.log(result);
+        }
+    });
+});
