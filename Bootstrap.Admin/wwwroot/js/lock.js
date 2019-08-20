d3b51bef2fbd852e15e35a5c89fde2ccdb356e08
Argo Zhang <Argo@163.com>
2019-08-20 18:19:45 +0800

    refactor: 移除js脚本设置样式代码

--- Bootstrap.Admin/wwwroot/js/lock.js
+++ Bootstrap.Admin/wwwroot/js/lock.js
@@ -1,12 +1,9 @@
 $(function () {
-    $('body').addClass('lock-screen');
     $('#time').text((new Date()).format('HH:mm:ss'));
 
     setInterval(function () {
         $('#time').text((new Date()).format('HH:mm:ss'));
     }, 500);
-
-    $.extend($.validator.messages, { required: "请输入密码" });
 
     $(".lock-wrapper").autoCenter();
 });
