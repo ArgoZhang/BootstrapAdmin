3b42178f2a1e07145accb0f6d4d18981f5048f7a
Argo Zhang <Argo@163.com>
2019-09-05 15:24:04 +0800

    build: fix script path issues

--- appveyor.build.ps1
+++ appveyor.build.ps1
@@ -6,7 +6,7 @@ runCmd "dotnet build src\admin\Bootstrap.Admin"
 runCmd "dotnet publish src\admin\Bootstrap.Admin --configuration Release --no-restore"
 
 $publishFolder = "$($env:appveyor_build_folder)\src\admin\Bootstrap.Admin\bin\Release\netcoreapp2.2\publish"
-$licFile = "$($env:appveyor_build_folder)\scripts\Longbow.lic"
+$licFile = "$($env:appveyor_build_folder)\src\admin\keys\Longbow.lic"
 write-host "copy file $licFile" -ForegroundColor Cyan
 xcopy $licFile $publishFolder /y
 
