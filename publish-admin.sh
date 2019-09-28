df1db5023d3e6e427341064afef4907d01b19e3a
Argo Zhang <Argo@163.com>
2019-09-28 22:53:16 +0800

    fix(build): 修正 linux 发布脚本

--- publish-admin.sh
+++ publish-admin.sh
@@ -4,8 +4,9 @@ cd ~/BootstrapAdmin
 git pull
 dotnet publish src/admin/Bootstrap.Admin -c Release
 
-rm -f ~/BootstrapAdmin/src/admin/Bootstrap.Admin/bin/Release/netcoreapp2.2/publish/appsettings*.json
+rm -f ~/BootstrapAdmin/src/admin/Bootstrap.Admin/bin/Release/netcoreapp3.0/publish/appsettings*.json
+rm -f ~/BootstrapAdmin/src/admin/Bootstrap.Admin/bin/Release/netcoreapp3.0/publish/BootstrapAdmin.db
 systemctl stop ba.admin
-\cp -fr ~/BootstrapAdmin/src/admin/Bootstrap.Admin/bin/Release/netcoreapp2.2/publish/* /usr/local/ba/admin/
+\cp -fr ~/BootstrapAdmin/src/admin/Bootstrap.Admin/bin/Release/netcoreapp3.0/publish/* /usr/local/ba/admin/
 systemctl start ba.admin
 systemctl status ba.admin
