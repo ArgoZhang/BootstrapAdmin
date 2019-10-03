c871b87e547cf702de3c215f8c2b7546ebfc168e
Argo Zhang <argo@163.com>
2019-10-03 23:14:53 +0800

    build(init): 增加 CentOS 服务脚本以及初始化 init.sh 命令

---
+++ init.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+mkdir /usr/local/ba
+mkdir /usr/local/ba/admin
+mkdir /usr/local/ba/client
+
+cd ~/
+git clone https://gitee.com/LongbowEnterprise/BootstrapAdmin.git
+
+cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/appsettings.json /usr/local/admin
+cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/BootstrapAdmin.db /usr/local/admin
+cp ~/BootstrapAdmin/src/client/Bootstrap.Client/appsettings.json /usr/local/client
+
+cp * /usr/lib/systemd/system/
+systemctl enable ba.admin
+systemctl enable ba.client
+systemctl enable nginx
