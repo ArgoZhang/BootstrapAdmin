b362a0b6830b4c6134cc1a483d7f6f3f4b6d166b
Argo Zhang <argo@163.com>
2019-10-03 23:55:22 +0800

    build(init): 删除 git clone 脚本

--- init.sh
+++ init.sh
@@ -4,9 +4,6 @@ mkdir /usr/local/ba
 mkdir /usr/local/ba/admin
 mkdir /usr/local/ba/client
 
-cd ~/
-git clone https://gitee.com/LongbowEnterprise/BootstrapAdmin.git
-
 cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/appsettings.json /usr/local/admin
 cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/BootstrapAdmin.db /usr/local/admin
 cp ~/BootstrapAdmin/src/client/Bootstrap.Client/appsettings.json /usr/local/client
