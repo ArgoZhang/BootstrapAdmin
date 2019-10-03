1ab3cee661e85b591db57bc7b8af2c7f1c726657
Argo Zhang <argo@163.com>
2019-10-03 23:43:41 +0800

    build: 更新发布命令

--- publish-admin.sh
+++ publish-admin.sh
@@ -5,6 +5,8 @@ git pull
 dotnet publish src/admin/Bootstrap.Admin -c Release
 
 rm -f ~/BootstrapAdmin/src/admin/Bootstrap.Admin/bin/Release/netcoreapp2.2/publish/appsettings*.json
+rm -f ~/BootstrapAdmin/src/admin/Bootstrap.Admin/bin/Release/netcoreapp2.2/publish/BootstrapAdmin.db
+
 systemctl stop ba.admin
 \cp -fr ~/BootstrapAdmin/src/admin/Bootstrap.Admin/bin/Release/netcoreapp2.2/publish/* /usr/local/ba/admin/
 systemctl start ba.admin
--- publish-client.sh
+++ publish-client.sh
@@ -5,6 +5,7 @@ git pull
 dotnet publish src/client/Bootstrap.Client -c Release
 
 rm -f ~/BootstrapAdmin/src/client/Bootstrap.Client/bin/Release/netcoreapp2.2/publish/appsettings*.json
+
 systemctl stop ba.client
 \cp -fr ~/BootstrapAdmin/src/client/Bootstrap.Client/bin/Release/netcoreapp2.2/publish/* /usr/local/ba/client/
 systemctl start ba.client
