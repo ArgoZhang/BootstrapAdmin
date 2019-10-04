7a9674187962af9a8fd186f7924c3d5276d9370c
Argo Zhang <argo@163.com>
2019-10-04 10:57:00 +0800

    build(deploy): 调整部署脚本

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/appsettings.json /usr/local/admin
 cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/BootstrapAdmin.db /usr/local/admin
 cp ~/BootstrapAdmin/src/client/Bootstrap.Client/appsettings.json /usr/local/client
 
-cp * /usr/lib/systemd/system/
+cp ~/BootstrapAdmin/services/* /usr/lib/systemd/system
 systemctl enable ba.admin
 systemctl enable ba.client
 systemctl enable nginx
