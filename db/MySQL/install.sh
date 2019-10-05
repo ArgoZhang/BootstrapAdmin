5399f00f5ac2fc09316682105249615b93a0fe96
Argo Zhang <Argo@163.com>
2019-10-05 20:36:12 +0800

    doc: 格式化数据库脚本

--- db/MySQL/install.sh
+++ db/MySQL/install.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 mysql  -e "drop database if exists BootstrapAdmin; create database BootstrapAdmin;" -uroot
 mysql -hlocalhost -uroot -DBootstrapAdmin < ~/src/Longbow/BootstrapAdmin/DatabaseScripts/MySQL/install.sql
 mysql -hlocalhost -uroot -DBootstrapAdmin < ~/src/Longbow/BootstrapAdmin/DatabaseScripts/MySQL/initData.sql
