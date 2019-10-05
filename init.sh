1046009edbd8e749ada5aa284870a0d9c58687e6
Argo Zhang <Argo@163.com>
2019-10-05 14:09:15 +0800

    build(init): 修复自动部署脚本路径错误
    # Conflicts:
    #	init.sh

--- init.sh
+++ init.sh
@@ -4,12 +4,9 @@ mkdir /usr/local/ba
 mkdir /usr/local/ba/admin
 mkdir /usr/local/ba/client
 
-cd ~/
-git clone https://gitee.com/LongbowEnterprise/BootstrapAdmin.git
-
-cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/appsettings.json /usr/local/admin
-cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/BootstrapAdmin.db /usr/local/admin
-cp ~/BootstrapAdmin/src/client/Bootstrap.Client/appsettings.json /usr/local/client
+cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/appsettings.json /usr/local/ba/admin
+cp ~/BootstrapAdmin/src/admin/Bootstrap.Admin/BootstrapAdmin.db /usr/local/ba/admin
+cp ~/BootstrapAdmin/src/client/Bootstrap.Client/appsettings.json /usr/local/ba/client
 
 cp ~/BootstrapAdmin/services/* /usr/lib/systemd/system
 systemctl enable ba.admin
