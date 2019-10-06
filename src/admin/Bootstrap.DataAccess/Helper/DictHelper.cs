7a1c75e4362947fdbd8aa69dba63f890c84b8f5e
Argo Zhang <Argo@163.com>
2019-10-06 22:12:51 +0800

    feat(#I12VKZ): 网站图标与前台一致
    #Comment
    comment #I12VKZ
    
    #Issue
    link #I12VKZ

--- src/admin/Bootstrap.DataAccess/Helper/DictHelper.cs
+++ src/admin/Bootstrap.DataAccess/Helper/DictHelper.cs
@@ -45,6 +45,19 @@ namespace Bootstrap.DataAccess
         }
 
         /// <summary>
+        /// 获取网站 logo 小图标
+        /// </summary>
+        /// <param name="appId"></param>
+        /// <returns></returns>
+        public static string RetrieveWebLogo(string appId)
+        {
+            // 获取应用程序 logo
+            var ditcs = RetrieveDicts();
+            var platName = ditcs.FirstOrDefault(d => d.Category == "应用程序" && d.Code == appId)?.Name;
+            return ditcs.FirstOrDefault(d => d.Category == platName && d.Name == "网站图标")?.Code ?? $"~/favicon.png";
+        }
+
+        /// <summary>
         /// 删除字典中的数据
         /// </summary>
         /// <param name="value">需要删除的IDs</param>
