c43d6be08957a35647b7d7c8beacc80b33845d27
Argo Zhang <Argo@163.com>
2019-10-09 23:08:55 +0800

    test: 增加 LogHelper.DbLogTask 单元测试

---
+++ test/UnitTest/Bootstrap.DataAccess/SQLite/LogHelperTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace Bootstrap.DataAccess.SQLite
+{
+    [Collection("SQLiteContext")]
+    public class LogHelperTest
+    {
+        [Fact]
+        public void Task_Ok()
+        {
+            var log = new DBLog()
+            {
+                Id = "",
+                LogTime = DateTime.Now,
+                SQL = "UnitTest",
+                UserName = "Admin"
+            };
+            LogHelper.AddDBLog(log);
+            var task = new LogHelper.DbLogTask();
+            task.Execute(CancellationToken.None);
+        }
+    }
+}
