9484a6e598b2e1b88d1576b609fed6e9f2aa3bb5
Argo Window10 <argo@163.com>
2019-11-28 17:50:46 +0800

    feat: 分页栏增加自定义每页数据数量

--- src/admin/Bootstrap.Admin/Components/PaginationBase.cs
+++ src/admin/Bootstrap.Admin/Components/PaginationBase.cs
@@ -1,8 +1,6 @@
-﻿using Bootstrap.Admin.Extensions;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System;
-using System.Threading.Tasks;
 
 namespace Bootstrap.Admin.Components
 {
@@ -18,7 +16,19 @@ namespace Bootstrap.Admin.Components
         public int PageCount { get; set; } = 0;
 
         /// <summary>
-        /// 
+        /// 获得/设置 每页显示数据数量
+        /// </summary>
+        [Parameter]
+        public int PageItems { get; set; } = 20;
+
+        /// <summary>
+        /// 获得/设置 数据总数
+        /// </summary>
+        [Parameter]
+        public int ItemsCount { get; set; } = 0;
+
+        /// <summary>
+        /// 获得/设置 当前页码
         /// </summary>
         [Parameter]
         public int PageIndex { get; set; } = 1;
@@ -32,14 +42,19 @@ namespace Bootstrap.Admin.Components
         /// <summary>
         /// 
         /// </summary>
-        public Action<int>? ClickPageCallback { get; set; }
+        public Action<int> ClickPageCallback { get; set; } = new Action<int>(i => { });
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Action PageItemsChangeCallback { get; set; } = new Action(() => { });
 
         /// <summary>
         /// 
         /// </summary>
         protected void MovePrev()
         {
-            if (PageIndex > 1) ClickPageCallback?.Invoke(PageIndex - 1);
+            if (PageIndex > 1) ClickPageCallback(PageIndex - 1);
         }
 
         /// <summary>
@@ -47,7 +62,17 @@ namespace Bootstrap.Admin.Components
         /// </summary>
         protected void MoveNext()
         {
-            if (PageIndex < PageCount) ClickPageCallback?.Invoke(PageIndex + 1);
+            if (PageIndex < PageCount) ClickPageCallback(PageIndex + 1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="pageItems"></param>
+        protected void ClickItem(int pageItems)
+        {
+            PageItems = pageItems;
+            PageItemsChangeCallback();
         }
     }
 }
