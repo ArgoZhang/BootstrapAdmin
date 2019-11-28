6004c0c8f19c1d669f9eefd8bf55b7028fa2966b
Argo Window10 <argo@163.com>
2019-11-28 15:22:01 +0800

    feat: 增加分页组件

---
+++ src/admin/Bootstrap.Admin/Components/PaginationBase.cs
@@ -0,0 +1,53 @@
+﻿using Bootstrap.Admin.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Bootstrap.Admin.Components
+{
+    /// <summary>
+    /// 分页组件基类
+    /// </summary>
+    public class PaginationBase : ComponentBase
+    {
+        /// <summary>
+        /// 获得/设置 页码总数
+        /// </summary>
+        [Parameter]
+        public int PageCount { get; set; } = 0;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public int PageIndex { get; set; } = 1;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Inject]
+        protected IJSRuntime? JSRuntime { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Action<int>? ClickPageCallback { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected void MovePrev()
+        {
+            if (PageIndex > 1) ClickPageCallback?.Invoke(PageIndex - 1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected void MoveNext()
+        {
+            if (PageIndex < PageCount) ClickPageCallback?.Invoke(PageIndex + 1);
+        }
+    }
+}
---
+++ src/admin/Bootstrap.Admin/Components/TableBase.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Collections.Generic;
+
+namespace Bootstrap.Admin.Components
+{
+    /// <summary>
+    /// 表格组件类
+    /// </summary>
+    public class TableBase<TItem> : ComponentBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Inject]
+        protected IJSRuntime? JSRuntime { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [Parameter]
+        public string Id { get; set; } = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public RenderFragment? TableHeader { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public RenderFragment<TItem>? RowTemplate { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public RenderFragment? TableFooter { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public IEnumerable<TItem> Items { get; set; } = new TItem[0];
+
+        /// <summary>
+        /// 数据
+        /// </summary>
+        [Parameter]
+        public int ItemsCount { get; set; } = 0;
+    }
+}
