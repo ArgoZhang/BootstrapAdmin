8bda9c416feeccd38bed095b747de66d30d0fd54
Argo Window10 <argo@163.com>
2019-11-28 17:07:13 +0800

    feat: 增加 Select 组件

---
+++ src/admin/Bootstrap.Admin/Components/SelectBase.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Bootstrap.Admin.Components
+{
+    /// <summary>
+    /// Select 组件基类
+    /// </summary>
+    public class SelectBase : ComponentBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string PlaceHolder { get; set; } = "请选择 ...";
+
+        /// <summary>
+        /// 当前选择项实例
+        /// </summary>
+        public SelectItemBase? SelectedItem { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Action<SelectItemBase>? ClickItemCallback { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Action<SelectItemBase>? ActiveChanged { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                ClickItemCallback = UpdateItem;
+                ActiveChanged = UpdateItem;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        protected void UpdateItem(SelectItemBase item)
+        {
+            SelectedItem = item;
+            StateHasChanged();
+        }
+    }
+}
---
+++ src/admin/Bootstrap.Admin/Components/SelectItemBase.cs
@@ -0,0 +1,55 @@
+﻿using Bootstrap.Admin.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Bootstrap.Admin.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SelectItemBase : ComponentBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public bool Active { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string Text { get; set; } = "";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string Value { get; set; } = "";
+
+        /// <summary>
+        ///
+        /// </summary>
+        [CascadingParameter]
+        public Select? Select { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                if (Active) Select?.ActiveChanged?.Invoke(this);
+            }
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        protected void ClickItem()
+        {
+            Select?.ClickItemCallback?.Invoke(this);
+        }
+    }
+}
