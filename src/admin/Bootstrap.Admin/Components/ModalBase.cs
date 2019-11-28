5b05c4b9ab888037f194d6fed4128ff7e187f11e
Argo Window10 <argo@163.com>
2019-11-28 11:49:13 +0800

    feat: Modal 组件模板化

---
+++ src/admin/Bootstrap.Admin/Components/ModalBase.cs
@@ -0,0 +1,60 @@
+﻿using Bootstrap.Admin.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Bootstrap.Admin.Components
+{
+    /// <summary>
+    /// 模态框组件类
+    /// </summary>
+    public class ModalBase : ComponentBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Inject]
+        protected IJSRuntime? JSRuntime { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [Parameter]
+        public string Id { get; set; } = "";
+
+        /// <summary>
+        ///
+        /// </summary>
+        [Parameter]
+        public string Title { get; set; } = "未设置";
+
+        /// <summary>
+        ///
+        /// </summary>
+        [Parameter]
+        public RenderFragment? ModalBody { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public bool Backdrop { get; set; }
+
+        /// <summary>
+        ///
+        /// </summary>
+        [Parameter]
+        public RenderFragment? ModalFooter { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                JSRuntime.InitModal();
+            }
+        }
+    }
+}
