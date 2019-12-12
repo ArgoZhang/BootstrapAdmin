210209b94df79f90660a39dee9a94e999c5cc7bc
Argo Window10 <argo@163.com>
2019-12-12 15:33:38 +0800

    build: 更新 linux 部署脚本

--- deploy-client.sh
+++ deploy-client.sh
@@ -4,8 +4,8 @@ cd ~/BootstrapAdmin
 git pull
 dotnet publish src/client/Bootstrap.Client -c Release
 
-rm -f ~/BootstrapAdmin/src/client/Bootstrap.Client/bin/Release/netcoreapp3.0/publish/appsettings*.json
+rm -f ~/BootstrapAdmin/src/client/Bootstrap.Client/bin/Release/netcoreapp3.1/publish/appsettings*.json
 systemctl stop ba.client
-\cp -fr ~/BootstrapAdmin/src/client/Bootstrap.Client/bin/Release/netcoreapp3.0/publish/* /usr/local/ba/client/
+\cp -fr ~/BootstrapAdmin/src/client/Bootstrap.Client/bin/Release/netcoreapp3.1/publish/* /usr/local/ba/client/
 systemctl start ba.client
 systemctl status ba.client -l
