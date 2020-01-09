1e3fabc46f66c456cf42eb96c09fb866ceed9ec8
Argo Zhang <Argo@163.com>
2020-01-09 12:35:38 +0800

    refactor: 增加 PageBase 基类

--- src/admin/Bootstrap.Admin/Pages/Components/DictsBase.cs
+++ src/admin/Bootstrap.Admin/Pages/Components/DictsBase.cs
@@ -1,35 +1,35 @@
-﻿using Bootstrap.Security;
-using Microsoft.AspNetCore.Components;
+﻿using Bootstrap.Admin.Components;
+using Bootstrap.Security;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Bootstrap.Admin.Components
+namespace Bootstrap.Pages.Admin.Components
 {
     /// <summary>
     /// 字典表维护组件
     /// </summary>
-    public class DictsBase : ComponentBase
+    public class DictsBase : PageBase
     {
         /// <summary>
-        /// 
+        /// 获得/设置 BootstrapDict 实例
         /// </summary>
         protected BootstrapDict QueryModel { get; set; } = new BootstrapDict() { Define = -1 };
 
         /// <summary>
-        /// 
+        /// 获得/设置 字典类别集合
         /// </summary>
         protected List<SelectedItem> DefineItems { get; set; } = new List<SelectedItem>(new SelectedItem[] { new SelectedItem() { Text = "系统使用", Value = "0" }, new SelectedItem() { Text = "自定义", Value = "1" } });
 
         /// <summary>
-        /// 
+        /// 获得/设置 查询条件集合
         /// </summary>
         protected List<SelectedItem> QueryDefine { get; set; } = new List<SelectedItem>(new SelectedItem[] { new SelectedItem() { Text = "全部", Value = "-1", Active = true }, new SelectedItem() { Text = "系统使用", Value = "0" }, new SelectedItem() { Text = "自定义", Value = "1" } });
 
         /// <summary>
-        /// 
+        /// 查询方法
         /// </summary>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageItems"></param>
+        /// <param name="pageIndex">页码</param>
+        /// <param name="pageItems">每页显示数据条目数量</param>
         protected QueryData<BootstrapDict> Query(int pageIndex, int pageItems)
         {
             var data = DataAccess.DictHelper.RetrieveDicts();
@@ -42,7 +42,7 @@ namespace Bootstrap.Admin.Components
         }
 
         /// <summary>
-        /// 
+        /// 新建方法
         /// </summary>
         /// <returns></returns>
         protected BootstrapDict Add()
@@ -51,19 +51,13 @@ namespace Bootstrap.Admin.Components
         }
 
         /// <summary>
-        /// 
+        /// 保存方法
         /// </summary>
         protected bool Save(BootstrapDict dict) => DataAccess.DictHelper.Save(dict);
 
         /// <summary>
-        /// 
+        /// 删除方法
         /// </summary>
         protected bool Delete(IEnumerable<BootstrapDict> items) => DataAccess.DictHelper.Delete(items.Select(item => item.Id ?? ""));
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        protected override bool ShouldRender() => false;
     }
 }
