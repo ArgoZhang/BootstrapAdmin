1ba3c042cfb86909a991501e7090bee2f7f7925c
Argo Zhang <Argo@163.com>
2020-01-15 15:07:46 +0800

    fix(#I18FEB): Blazor 模式下增加部门维护
    #Comment
    comment #I18FEB
    
    #Issue
    close https://gitee.com/LongbowEnterprise/dashboard/issues?id=I18FEB

---
+++ src/admin/Bootstrap.Admin/Pages/Components/GroupsBase.cs
@@ -0,0 +1,39 @@
+using Bootstrap.Admin.Components;
+using Bootstrap.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bootstrap.Pages.Admin.Components
+{
+    /// <summary>
+    /// 部门维护组件
+    /// </summary>
+    public class GroupsBase : QueryPageBase<Group>
+    {
+        /// <summary>
+        /// 查询方法
+        /// </summary>
+        /// <param name="pageIndex">页码</param>
+        /// <param name="pageItems">每页显示数据条目数量</param>
+        protected override QueryData<Group> Query(int pageIndex, int pageItems)
+        {
+            var data = GroupHelper.Retrieves();
+            if (!string.IsNullOrEmpty(QueryModel.GroupName)) data = data.Where(d => d.GroupName.Equals(QueryModel.GroupName, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrEmpty(QueryModel.Description)) data = data.Where(d => d.Description != null && d.Description.Contains(QueryModel.Description, StringComparison.OrdinalIgnoreCase));
+            var totalCount = data.Count();
+            var items = data.Skip((pageIndex - 1) * pageItems).Take(pageItems);
+            return new QueryData<Group>() { Items = items, TotalCount = totalCount, PageIndex = pageIndex, PageItems = pageItems };
+        }
+
+        /// <summary>
+        /// 保存方法
+        /// </summary>
+        protected override bool Save(Group group) => GroupHelper.Save(group);
+
+        /// <summary>
+        /// 删除方法
+        /// </summary>
+        protected override bool Delete(IEnumerable<Group> groups) => GroupHelper.Delete(groups.Select(item => item.Id ?? ""));
+    }
+}
