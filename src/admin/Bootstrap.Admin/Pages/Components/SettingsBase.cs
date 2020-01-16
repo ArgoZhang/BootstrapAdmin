bae10baf5d55a8d4ecc09a0e4f85999d236b1427
Argo-2016 <argo@163.com>
2020-01-16 16:12:36 +0800

    feat: 增加 Bootstrap-Toggle 组件

--- src/admin/Bootstrap.Admin/Pages/Components/SettingsBase.cs
+++ src/admin/Bootstrap.Admin/Pages/Components/SettingsBase.cs
@@ -1,5 +1,7 @@
-using Bootstrap.Admin.Models;
-using Bootstrap.Admin.Shared;
+﻿using Bootstrap.Admin.Shared;
+using Bootstrap.DataAccess;
+using System.Collections.Generic;
+using Bootstrap.Security;
 using Microsoft.AspNetCore.Components;
 
 namespace Bootstrap.Pages.Admin.Components
@@ -10,9 +12,9 @@ namespace Bootstrap.Pages.Admin.Components
     public class SettingsBase : ComponentBase
     {
         /// <summary>
-        /// 获得 SettingsModel 实例
+        /// 获得 EditModel 实例
         /// </summary>
-        protected SettingsModel? Model { get; set; }
+        protected EditModel Model { get; set; } = new EditModel();
 
         /// <summary>
         /// 获得/设置 默认母版页实例
@@ -23,9 +25,85 @@ namespace Bootstrap.Pages.Admin.Components
         /// <summary>
         /// 设置参数方法
         /// </summary>
-        protected override void OnParametersSet()
+        protected override void OnInitialized()
         {
-            Model = new SettingsModel(RootLayout?.UserName);
+            Model.Title = RootLayout?.Model.Title ?? "";
+            Model.Footer = RootLayout?.Model.Footer ?? "";
+            Model.Theme = RootLayout?.Model.Theme ?? "";
+            Model.ShowSideBar = DictHelper.RetrieveSidebarStatus();
+            Model.ShowCardTitle = DictHelper.RetrieveCardTitleStatus();
+            Model.ShowMobile = DictHelper.RetrieveMobileLogin();
+            Model.ShowOAuth = DictHelper.RetrieveOAuthLogin();
+            Model.AutoLockScreen = DictHelper.RetrieveAutoLockScreen();
+            Model.LockScreenPeriod = DictHelper.RetrieveAutoLockScreenPeriod();
+            Model.DefaultApp = DictHelper.RetrieveDefaultApp();
+            Model.EnableBlazor = DictHelper.RetrieveEnableBlazor();
+        }
+
+        /// <summary>
+        /// 网站设置编辑模型实体类
+        /// </summary>
+        protected class EditModel
+        {
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public string Title { get; set; } = "";
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public string Footer { get; set; } = "";
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public string Theme { get; set; } = "";
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public bool ShowSideBar { get; set; }
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public bool ShowCardTitle { get; set; }
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public bool ShowMobile { get; set; }
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public bool ShowOAuth { get; set; }
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public bool AutoLockScreen { get; set; }
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public int LockScreenPeriod { get; set; }
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public bool DefaultApp { get; set; }
+
+            /// <summary>
+            /// 获得/设置
+            /// </summary>
+            public bool EnableBlazor { get; set; }
+
+            /// <summary>
+            /// 获得/设置 系统样式集合
+            /// </summary>
+            public IEnumerable<BootstrapDict> Themes { get; set; } = new HashSet<BootstrapDict>();
         }
     }
 }
