dca64218c1a47be77df9cf921f111e99e6d015d1
Argo Zhang <Argo@163.com>
2020-01-23 17:09:56 +0800

    refactor: 消除可为空警告信息

--- src/admin/Bootstrap.Admin/Pages/Components/GroupsBase.cs
+++ src/admin/Bootstrap.Admin/Pages/Components/GroupsBase.cs
@@ -22,7 +22,7 @@ namespace Bootstrap.Pages.Admin.Components
             var data = GroupHelper.Retrieves();
             if (!string.IsNullOrEmpty(QueryModel.GroupName)) data = data.Where(d => d.GroupName.Contains(QueryModel.GroupName, StringComparison.OrdinalIgnoreCase));
             if (!string.IsNullOrEmpty(QueryModel.Description)) data = data.Where(d => d.Description != null && d.Description.Contains(QueryModel.Description, StringComparison.OrdinalIgnoreCase));
-            if (!string.IsNullOrEmpty(searchText)) data = data.Where(d => d.GroupName.Contains(searchText, StringComparison.OrdinalIgnoreCase) || d.Description.Contains(searchText, StringComparison.OrdinalIgnoreCase));
+            if (!string.IsNullOrEmpty(searchText)) data = data.Where(d => d.GroupName.Contains(searchText, StringComparison.OrdinalIgnoreCase) || (d.Description ?? "").Contains(searchText, StringComparison.OrdinalIgnoreCase));
             var totalCount = data.Count();
             var items = data.Skip((pageIndex - 1) * pageItems).Take(pageItems);
             return new QueryData<Group>() { Items = items, TotalCount = totalCount, PageIndex = pageIndex, PageItems = pageItems };
