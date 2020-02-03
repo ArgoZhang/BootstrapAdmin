3a8621825dcc416613dbb523965159e1094960a5
Argo Zhang <Argo@163.com>
2020-02-03 17:32:12 +0800

    feat: Blazor 增加登录日志功能

--- src/admin/Bootstrap.DataAccess/LoginUser.cs
+++ src/admin/Bootstrap.DataAccess/LoginUser.cs
@@ -2,6 +2,7 @@
 using PetaPoco;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Bootstrap.DataAccess
 {
@@ -19,41 +20,49 @@ namespace Bootstrap.DataAccess
         /// <summary>
         /// 获得/设置 用户名
         /// </summary>
+        [DisplayName("登录名称")]
         public string UserName { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 登录时间
         /// </summary>
+        [DisplayName("登录时间")]
         public DateTime LoginTime { get; set; }
 
         /// <summary>
         /// 获得/设置 登录IP地址
         /// </summary>
+        [DisplayName("主机")]
         public string Ip { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 登录浏览器
         /// </summary>
+        [DisplayName("浏览器")]
         public string Browser { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 登录操作系统
         /// </summary>
+        [DisplayName("操作系统")]
         public string OS { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 登录地点
         /// </summary>
+        [DisplayName("登录地点")]
         public string City { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 登录是否成功
         /// </summary>
+        [DisplayName("登录结果")]
         public string Result { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 用户 UserAgent
         /// </summary>
+        [DisplayName("登录名称")]
         public string UserAgent { get; set; } = "";
 
         /// <summary>
