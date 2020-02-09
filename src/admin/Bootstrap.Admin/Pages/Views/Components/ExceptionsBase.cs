43b48fc9ae1f84d01e75a167b87037274d829124
Argo-2016 <argo@163.com>
2020-02-09 13:28:21 +0800

    feat: Blazor 程序异常页面增加文件日志明细功能

--- src/admin/Bootstrap.Admin/Pages/Views/Components/ExceptionsBase.cs
+++ src/admin/Bootstrap.Admin/Pages/Views/Components/ExceptionsBase.cs
@@ -3,6 +3,7 @@ using Bootstrap.Admin.Pages.Extensions;
 using Bootstrap.DataAccess;
 using Microsoft.AspNetCore.Components;
 using System;
+using System.Collections.Generic;
 
 namespace Bootstrap.Admin.Pages.Views.Admin.Components
 {
@@ -51,9 +52,42 @@ namespace Bootstrap.Admin.Pages.Views.Admin.Components
         }
 
         /// <summary>
+        /// 获得 错误日志文件集合
+        /// </summary>
+        protected IEnumerable<string> Items { get; set; } = new string[0];
+
+        private bool show;
+        /// <summary>
         /// 显示异常明细方法
         /// </summary>
         protected void ShowDetail()
+        {
+            Items = ExceptionsHelper.RetrieveLogFiles();
+            show = true;
+            StateHasChanged();
+        }
+
+        /// <summary>
+        /// OnAfterRender 方法
+        /// </summary>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (show)
+            {
+                show = false;
+                Modal?.Toggle();
+            }
+        }
+
+        /// <summary>
+        /// 获得/设置 Modal 实例
+        /// </summary>
+        protected ModalBase? Modal { get; set; }
+
+        /// <summary>
+        /// 显示指定文件内容明细
+        /// </summary>
+        protected void ShowLog(string fileName)
         {
 
         }
