722b2e1a5288bd9af9e20e68ce96427cb11c64df
Argo-2016 <argo@163.com>
2020-02-13 12:06:36 +0800

    build: 增加 MySql 单元测试

---
+++ appveyor.install.ps1
@@ -0,0 +1,13 @@
+﻿# 显示 dotnet version
+write-host "dotnet --version" -ForegroundColor Cyan
+dotnet --version
+
+# 注意 my.ini 文件行结束符必须为CRLF
+
+$iniPath="C:\Program Files\MySQL\MySQL Server 5.7\my.ini"
+write-host "copy $($env:appveyor_build_folder)\db\MySQL\my.ini -> $iniPath" -ForegroundColor Cyan
+xcopy "$($env:appveyor_build_folder)\db\MySQL\my.ini" $iniPath /y
+$newText = ([System.IO.File]::ReadAllText($iniPath)).Replace("\n", "\r\n")
+[System.IO.File]::WriteAllText($iniPath, $newText)
+
+write-host "starting database services ..." -ForegroundColor Cyan
