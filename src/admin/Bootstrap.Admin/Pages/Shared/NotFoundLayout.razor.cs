2b5e8e0c938c2ad7a458d38ffcf507bf516502c5
Argo-Tianyi <argo@163.com>
2021-05-05 11:31:50 +0800

    refactor: 修复 NotFoundLayout 组件

---
+++ src/admin/Bootstrap.Admin/Pages/Shared/NotFoundLayout.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Bootstrap.Admin.Models;
+
+namespace Bootstrap.Admin.Pages.Shared
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class NotFoundLayout
+    {
+        [NotNull]
+        private ErrorModel? Model { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            var id = 404;
+            Model = ErrorModel.CreateById(id);
+        }
+    }
+}
