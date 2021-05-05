e6782c40dbf3f33fa5eb2cb7297d3b7c0061ca36
Argo-Tianyi <argo@163.com>
2021-05-05 11:05:19 +0800

    feat: 增加重定向组件

---
+++ src/admin/Bootstrap.Admin/Pages/Shared/RedirectToLogin.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootstrap.Admin.Pages.Shared
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RedirectToLogin : ComponentBase
+    {
+        [Inject]
+        [NotNull]
+        private NavigationManager? Navigation { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            Navigation.NavigateTo("/Account/Login", true);
+        }
+    }
+}
