ddab6ea72548fcffa7a043c11e2a7b6f21e6ab86
zhangpeihang <948869991@qq.com>
2021-11-25 15:47:47 +0800

    feat: 新增字典表

---
+++ src/admin/Bootstrap.Admin.Blazor/Pages/Dicts.razor.cs
@@ -0,0 +1,39 @@
+﻿using Bootstrap.Security;
+using BootstrapBlazor.Components;
+using System.Linq;
+
+namespace Bootstrap.Admin.Blazor.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class Dicts
+    {
+        private Task<(IEnumerable<BootstrapDict>, int)> QueryAsyncCallback(QueryPageOptions options)
+        {
+            var items = DataAccess.DictHelper.RetrieveDicts();
+            if (options.Filters.Any())
+            {
+                items.Where(options.Filters.GetFilterFunc<BootstrapDict>());
+            }
+            var total = DataAccess.DictHelper.RetrieveDicts().Count();
+
+
+            var pageItems = items.Skip((options.PageIndex - 1) * options.PageItems).Take(options.PageItems);
+
+            return Task.FromResult((pageItems, total));
+        }
+
+        private Task OnCustomterColumnCreating(List<ITableColumn> columns)
+        {
+            foreach (var item in columns)
+            {
+                item.Searchable = true;
+                item.Sortable = true;
+                item.Filterable = true;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
