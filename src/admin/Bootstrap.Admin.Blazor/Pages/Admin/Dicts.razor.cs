12eacc37bca88b413023fdcd99990a838cca8444
zhangpeihang <948869991@qq.com>
2021-11-26 21:30:51 +0800

    feat: 自定义 字典类型显示模板、编辑模板、搜索模板

--- src/admin/Bootstrap.Admin.Blazor/Pages/Admin/Dicts.razor.cs
+++ src/admin/Bootstrap.Admin.Blazor/Pages/Admin/Dicts.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Bootstrap.Security;
 using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
 
 namespace Bootstrap.Admin.Blazor.Pages.Admin
 {
@@ -8,6 +9,30 @@ namespace Bootstrap.Admin.Blazor.Pages.Admin
     /// </summary>
     public partial class Dicts
     {
+        private IEnumerable<SelectedItem>? Categories { get; set; }
+
+        private IEnumerable<SelectedItem>? EditCategories { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Categories = new List<SelectedItem>()
+            {
+                new SelectedItem("","全部"){ Active = true},
+                new SelectedItem("0","系统使用"),
+                new SelectedItem("1","自定义"),
+            };
+
+            EditCategories = new List<SelectedItem>()
+            {
+                new SelectedItem("0","系统使用"),
+                new SelectedItem("1","自定义"),
+            };
+        }
+
         private Task<(IEnumerable<BootstrapDict>, int)> QueryAsyncCallback(QueryPageOptions options)
         {
             var items = DataAccess.DictHelper.RetrieveDicts();
@@ -19,5 +44,12 @@ namespace Bootstrap.Admin.Blazor.Pages.Admin
             }
             return Task.FromResult((items, total));
         }
+
+        private string CreateComponent(int code) => code switch
+        {
+            0 => "<div>系统使用</div>",
+            1 => "<div>自定义</div>",
+            _ => ""
+        };
     }
 }
