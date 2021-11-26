1b1503af3c440e22a1f587899cf267194feec0b0
zhangpeihang <948869991@qq.com>
2021-11-26 21:45:36 +0800

    feat: 加载管理员菜单

--- src/admin/Bootstrap.Admin.Blazor/Shared/AdminLayout.razor.cs
+++ src/admin/Bootstrap.Admin.Blazor/Shared/AdminLayout.razor.cs
@@ -1,10 +1,22 @@
-﻿namespace Bootstrap.Admin.Blazor.Shared
+﻿using BootstrapBlazor.Components;
+
+namespace Bootstrap.Admin.Blazor.Shared
 {
     /// <summary>
     /// AdminLayout 布局类
     /// </summary>
     public partial class AdminLayout
     {
+        private IEnumerable<MenuItem>? MenuItems { get; set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            MenuItems = DataAccess.MenuHelper.RetrieveSystemMenus("Admin", "admin/dicts").Select(s => new MenuItem() { Url = s.Url.Replace("~", "").ToLower(), Text = s.Name, Icon = s.Icon, Items = s.Menus.Select(x => new MenuItem { Url = x.Url.Replace("~", "").ToLower(), Text = x.Name, Icon = x.Icon }) });
+        }
     }
 }
