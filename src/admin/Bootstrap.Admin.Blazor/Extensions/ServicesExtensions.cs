483bba5f43d48cddc2112612d1c4c0b43ba9d15c
Argo-Lenovo <argo@163.com>
2021-11-26 11:08:48 +0800

    chore: 更新项目框架

---
+++ src/admin/Bootstrap.Admin.Blazor/Extensions/ServicesExtensions.cs
@@ -0,0 +1,84 @@
+﻿using Bootstrap.DataAccess;
+using BootstrapBlazor.Components;
+using Longbow.Web.SignalR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ServicesExtensions
+    {
+        /// <summary>
+        /// 添加示例后台任务
+        /// </summary>
+        /// <param name="services"></param>
+        public static IServiceCollection AddBootstrapBlazorAdminServices(this IServiceCollection services)
+        {
+            // 增加后台任务服务
+            services.AddTaskServices();
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            services.AddLogging(logging => logging.AddFileLogger().AddCloudLogger().AddDBLogger(ExceptionsHelper.Log));
+            services.AddCors();
+            services.AddResponseCompression();
+
+            services.AddCodePageProvider();
+            services.AddCacheManager();
+            services.AddDbAdapter();
+            services.AddIPLocator(DictHelper.ConfigIPLocator);
+            services.AddOnlineUsers();
+            services.AddSignalR().AddJsonProtocol(op => op.PayloadSerializerOptions.AddDefaultConverters());
+            //services.AddBootstrapAdminAuthentication(Configuration)
+            //    .AddGitee(OAuthHelper.Configure)
+            //    .AddGitHub(OAuthHelper.Configure)
+            //    .AddTencent(OAuthHelper.Configure)
+            //    .AddAlipay(OAuthHelper.Configure);
+            //services.AddAuthorization(options => options.DefaultPolicy = new AuthorizationPolicyBuilder().RequireBootstrapAdminAuthorizate().Build());
+            //services.AddButtonAuthorization(MenuHelper.AuthorizateButtons);
+            services.AddBootstrapAdminBackgroundTask();
+            //services.AddHttpClient<GiteeHttpClient>();
+            //services.AddAdminHealthChecks();
+            //services.AddSMSProvider();
+
+            //services.AddSwagger();
+            //services.AddApiVersioning(option =>
+            //{
+            //    option.DefaultApiVersion = new ApiVersion(1, 0);
+            //    option.ReportApiVersions = true;
+            //    option.AssumeDefaultVersionWhenUnspecified = true;
+            //    option.ApiVersionReader = ApiVersionReader.Combine(new HeaderApiVersionReader("api-version"), new QueryStringApiVersionReader("api-version"));
+            //});
+            //services.AddExceptionless();
+            //services.AddControllersWithViews(options =>
+            //{
+            //    options.Filters.Add<BootstrapAdminAuthorizeFilter>();
+            //    options.Filters.Add<ExceptionFilter>();
+            //    options.Filters.Add<SignalRExceptionFilter<SignalRHub>>();
+            //}).AddJsonOptions(op => op.JsonSerializerOptions.AddDefaultConverters());
+            services.AddControllers().AddJsonOptions(op => op.JsonSerializerOptions.AddDefaultConverters());
+            services.AddDisplayNames();
+            // 增加 BootstrapBlazor 组件
+            services.AddBootstrapBlazor();
+
+            // 增加 PetaPoco ORM 数据服务操作类
+            // 需要时打开下面代码
+            //services.AddPetaPoco(option =>
+            //{
+            //    // 配置数据信息
+            //    // 使用 SQLite 数据以及从配置文件中获取数据库连接字符串
+            //    // 需要引用 Microsoft.Data.Sqlite 包，操作 SQLite 数据库
+            //    // 需要引用 PetaPoco.Extensions 包，PetaPoco 包扩展批量插入与删除
+            //    option.UsingProvider<SQLiteDatabaseProvider>()
+            //          .UsingConnectionString(Configuration.GetConnectionString("bb"));
+            //});
+
+            return services;
+        }
+    }
+}
--- src/admin/Bootstrap.Admin.Blazor/Program.cs
+++ src/admin/Bootstrap.Admin.Blazor/Program.cs
@@ -1,36 +1,13 @@
-﻿using BootstrapBlazor.DataAcces.PetaPoco;
-using BootstrapBlazor.Components;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.Extensions.Options;
-using Microsoft.Extensions.DependencyInjection;
-using PetaPoco.Providers;
-using PetaPoco;
-using Bootstrap.Admin.Blazor.Extensions;
+﻿using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddCacheManager();
-builder.Services.AddDbAdapter();
-builder.Services.AddBootstrapBlazor();
-builder.Services.AddRequestLocalization<IOptions<BootstrapBlazorOptions>>((localizerOption, blazorOption) =>
-{
-    var supportedCultures = blazorOption.Value.GetSupportedCultures();
 
-    localizerOption.SupportedCultures = supportedCultures;
-    localizerOption.SupportedUICultures = supportedCultures;
-});
-builder.Services.AddTableDataService();
-builder.Services.AddPetaPoco(option =>
-{
-    // 配置数据信息
-    // 使用 SQLite 数据以及从配置文件中获取数据库连接字符串
-    option.UsingProvider<SQLiteDatabaseProvider>()
-          .UsingConnectionString(builder.Configuration.GetConnectionString("ba"));
-});
+// 注入项目服务
+builder.Services.AddBootstrapBlazorAdminServices();
 
 var app = builder.Build();
 
@@ -40,12 +17,15 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
 }
 
-app.UseRequestLocalization(app.Services.GetService<IOptions<RequestLocalizationOptions>>()!.Value);
-
 app.UseStaticFiles();
 
 app.UseRouting();
 
+// 开启缓存中间件
+app.UseCacheManager();
+
+// 开启 webapi
+app.MapDefaultControllerRoute();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
