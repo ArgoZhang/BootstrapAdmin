f1a5cbf5060c1bf0189926c4ed76073bc3649a1c
zhangpeihang <948869991@qq.com>
2021-12-03 13:28:37 +0800

    refactor: 重构 user 页面

--- src/admin/Bootstrap.Admin.Blazor/Pages/Admin/Users.razor.cs
+++ src/admin/Bootstrap.Admin.Blazor/Pages/Admin/Users.razor.cs
@@ -1,6 +1,8 @@
 ﻿using BootstrapBlazor.Components;
 using Bootstrap.DataAccess;
 using Task = System.Threading.Tasks.Task;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Bootstrap.Admin.Blazor.Pages.Admin
 {
@@ -9,28 +11,59 @@ namespace Bootstrap.Admin.Blazor.Pages.Admin
     /// </summary>
     public partial class Users
     {
-        private Task<(IEnumerable<User>, int)> OnQueryAsync(QueryPageOptions options)
+        [Inject]
+        [NotNull]
+        private DialogService? DialogService { get; set; }
+
+        [Inject]
+        [NotNull]
+        private ToastService? ToastService { get; set; }
+
+        private Task<IEnumerable<User>> OnQueryAsync()
         {
-            var items = UserHelper.Retrieves();
-            var total = items.Count();
+            return Task.FromResult(UserHelper.Retrieves());
+        }
 
-            // 处理高级搜索
-            if (options.Searchs.Any())
+        private async Task OnAssignmentDept(IEnumerable<User> users)
+        {
+            if (users.Count() != 0)
             {
-                items = items.Where(options.Searchs.GetFilterFunc<User>());
-            }
+                var option = new DialogOption()
+                {
+                    Title = "部门授权",
+                    BodyTemplate = BootstrapDynamicComponent.CreateComponent<CheckboxList<string>>(new Dictionary<string, object>
+                    {
+                        [nameof(CheckboxList<string>.Items)] = GroupHelper.Retrieves().Select(s => new SelectedItem(s.GroupCode, s.GroupName))
+                    }).Render()
+                };
 
-            if (!string.IsNullOrEmpty(options.SortName))
-            {
-                items = items.Sort(options.SortName, options.SortOrder);
+                await DialogService.Show(option);
             }
+            else
+            {
+                await ToastService.Warning("部门授权", "请选择一个用户");
+            }
+        }
 
-            if (options.Filters.Any())
+        private async Task OnAssignmentRoles(IEnumerable<User> users)
+        {
+            if (users.Count() != 0)
             {
-                var aa = options.Filters.GetFilterFunc<User>();
-                items = items.Where(options.Filters.GetFilterFunc<User>());
+                var option = new DialogOption()
+                {
+                    Title = "分配角色",
+                    BodyTemplate = BootstrapDynamicComponent.CreateComponent<CheckboxList<string>>(new Dictionary<string, object>
+                    {
+                        [nameof(CheckboxList<string>.Items)] = RoleHelper.Retrieves().Select(s => new SelectedItem(s.Id!, s.RoleName) { Active = s.Checked == "" ? false : true })
+                    }).Render()
+                };
+
+                await DialogService.Show(option);
             }
-            return Task.FromResult((items, total));
+            else
+            {
+                await ToastService.Warning("分配角色", "请选择一个用户");
+            }
         }
     }
 }
