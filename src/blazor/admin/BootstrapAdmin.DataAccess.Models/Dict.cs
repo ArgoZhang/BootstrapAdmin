bf4fedc544ede2fdd1eeb048e1f2af28398aafff
Argo-Tianyi <argo@163.com>
2021-12-12 20:54:40 +0800

    refactor: 增加 Display 标签

--- src/blazor/admin/BootstrapAdmin.DataAccess.Models/Dict.cs
+++ src/blazor/admin/BootstrapAdmin.DataAccess.Models/Dict.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
 
+using System.ComponentModel.DataAnnotations;
+
 namespace BootstrapAdmin.DataAccess.Models
 {
     /// <summary>
@@ -13,23 +15,30 @@ namespace BootstrapAdmin.DataAccess.Models
         public string? Id { get; set; }
 
         /// <summary>
-        /// 获得/设置 字典分类
+        /// 获得/设置 字典标签
         /// </summary>
+        [Required(ErrorMessage = "{0}不可为空")]
+        [Display(Name = "字典标签")]
         public string Category { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 字典名称
         /// </summary>
+        [Required(ErrorMessage = "{0}不可为空")]
+        [Display(Name = "字典名称")]
         public string Name { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 字典字典值
         /// </summary>
+        [Required(ErrorMessage = "{0}不可为空")]
+        [Display(Name = "字典代码")]
         public string Code { get; set; } = "";
 
         /// <summary>
         /// 获得/设置 字典定义值 0 表示系统使用，1 表示用户自定义 默认为 1
         /// </summary>
+        [Display(Name = "字典类型")]
         public EnumDictDefine Define { get; set; } = EnumDictDefine.Customer;
     }
 }
