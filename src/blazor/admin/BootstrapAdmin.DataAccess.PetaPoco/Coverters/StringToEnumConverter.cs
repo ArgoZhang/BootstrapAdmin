5bc293dd50b58d370a13280bb418dd3979db3266
Argo-Tianyi <argo@163.com>
2021-12-12 18:58:04 +0800

    feat: 增加字符串转枚举转化器

---
+++ src/blazor/admin/BootstrapAdmin.DataAccess.PetaPoco/Coverters/StringToEnumConverter.cs
@@ -0,0 +1,59 @@
+﻿namespace BootstrapAdmin.DataAccess.PetaPoco.Coverters
+{
+    /// <summary>
+    /// 字符串转枚举转换器
+    /// </summary>
+    public class StringToEnumConverter
+    {
+        private Type TargetType { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="targetType"></param>
+        public StringToEnumConverter(Type targetType) => TargetType = targetType;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object? ConvertFromDb(object value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            object? ret;
+            if (value != null && Enum.TryParse(TargetType, value.ToString(), true, out var v))
+            {
+                ret = v;
+            }
+            else
+            {
+                throw new InvalidCastException($"{value} 无法转化为 {TargetType.Name} 成员");
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public object ConvertToDb(object value)
+        {
+            object? ret;
+            var field = value?.ToString();
+            if (!string.IsNullOrEmpty(field) && Enum.IsDefined(TargetType, field))
+            {
+                ret = field;
+            }
+            else
+            {
+                ret = Enum.GetNames(TargetType).First();
+            }
+            return ret;
+        }
+    }
+}
