6b58a7e850b3beef43a9da31b986392445fee0fc
Argo-Lenovo <argo@163.com>
2021-12-14 20:45:41 +0800

    refactor: 增加后台文件

---
+++ src/blazor/admin/BootstrapAdmin.Web/Components/DictSearch.razor.cs
@@ -0,0 +1,26 @@
+﻿using BootstrapAdmin.DataAccess.Models;
+using BootstrapAdmin.Web.Models;
+
+namespace BootstrapAdmin.Web.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class DictSearch
+    {
+        private IEnumerable<SelectedItem>? Items { get; set; } = typeof(EnumDictDefine).ToSelectList(new SelectedItem("", "全部"));
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        [NotNull]
+        public DictsSearchModel? Value { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public EventCallback<DictsSearchModel> ValueChanged { get; set; }
+    }
+}
