08d5e498a006912bd6109bb45e10daec75d350a7
Argo-Tianyi <argo@163.com>
2021-12-15 12:37:57 +0800

    feat: 更新 Home 首页逻辑

---
+++ src/blazor/admin/BootstrapAdmin.Web/Pages/Home/Index.cs
@@ -0,0 +1,31 @@
+﻿namespace BootstrapAdmin.Web.Pages.Home
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("/")]
+    [Route("/Home")]
+    [Route("/Home/Index")]
+    public class Index : ComponentBase
+    {
+        [Inject]
+        [NotNull]
+        private NavigationManager? Navigation { get; set; }
+
+#if DEBUG
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            Navigation.NavigateTo($"/Admin/Index", true);
+        }
+#else
+        protected override void OnInitialized()
+        {
+            Navigation.NavigateTo($"/Admin/Index", true);
+        }
+#endif
+    }
+}
