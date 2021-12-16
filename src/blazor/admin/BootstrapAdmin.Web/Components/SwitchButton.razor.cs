fd2fca1d90ccde4065f6c97743e0ce74bd9fef1b
Argo-Tianyi <argo@163.com>
2021-12-16 14:04:45 +0800

    feat: 增加 SwitchButton 组件

---
+++ src/blazor/admin/BootstrapAdmin.Web/Components/SwitchButton.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BootstrapAdmin.Web.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class SwitchButton
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? OnText { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? OffText { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public bool ToggleState { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public EventCallback<bool> ToggleStateChanged { get; set; }
+
+        private async Task Toggle()
+        {
+            ToggleState = !ToggleState;
+            if (ToggleStateChanged.HasDelegate)
+            {
+                await ToggleStateChanged.InvokeAsync(ToggleState);
+            }
+        }
+
+        private string? GetText() => ToggleState ? OnText : OffText;
+    }
+}
