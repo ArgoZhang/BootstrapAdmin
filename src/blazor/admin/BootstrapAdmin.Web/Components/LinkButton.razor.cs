f21df83abcbb0e6e82607da7818e3e1f6f18281e
Argo-Tianyi <argo@163.com>
2021-12-16 14:04:59 +0800

    feat: 增加 LinkButton 组件

---
+++ src/blazor/admin/BootstrapAdmin.Web/Components/LinkButton.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace BootstrapAdmin.Web.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class LinkButton
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? Text { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+    }
+}
