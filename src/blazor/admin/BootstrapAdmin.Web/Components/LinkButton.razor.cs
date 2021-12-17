972c9da4cfad5d297c8562f69bfa9d53cd2568fa
Argo-Tianyi <argo@163.com>
2021-12-17 10:03:01 +0800

    feat: 更新 LinkButton 精简代码

--- src/blazor/admin/BootstrapAdmin.Web/Components/LinkButton.razor.cs
+++ src/blazor/admin/BootstrapAdmin.Web/Components/LinkButton.razor.cs
@@ -17,6 +17,38 @@ namespace BootstrapAdmin.Web.Components
         /// 
         /// </summary>
         [Parameter]
+        public string Url { get; set; } = "#";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? Title { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? Img { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public Placement TooltipPlacement { get; set; } = Placement.Top;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
         public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        private bool Prevent => Url.StartsWith('#');
     }
 }
