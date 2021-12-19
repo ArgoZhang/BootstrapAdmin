4b3a61ed39930ece57d1f17bb4d0ed9325145d0a
Argo-Lenovo <argo@163.com>
2021-12-20 00:13:02 +0800

    feat: 完善菜单维护界面

--- src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Menus.razor.cs
+++ src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Menus.razor.cs
@@ -1,6 +1,25 @@
-﻿namespace BootstrapAdmin.Web.Pages.Admin
+﻿using BootstrapAdmin.Web.Components;
+
+namespace BootstrapAdmin.Web.Pages.Admin
 {
     public partial class Menus
     {
+        [Inject]
+        [NotNull]
+        private DialogService? DialogService { get; set; }
+
+        private async Task OnAssignmentRoles(DataAccess.Models.Navigation menu)
+        {
+            var option = new DialogOption()
+            {
+                Title = $"分配角色 - {menu}",
+                Component = BootstrapDynamicComponent.CreateComponent<MenuRole>(new Dictionary<string, object>
+                {
+                    [nameof(MenuRole.MenuId)] = menu.Id!
+                })
+            };
+
+            await DialogService.Show(option);
+        }
     }
 }
