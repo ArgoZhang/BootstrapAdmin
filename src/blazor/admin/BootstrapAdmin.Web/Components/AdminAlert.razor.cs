578bfc67105048de35416555954a5c2391d56521
Argo-Tianyi <argo@163.com>
2021-12-25 12:35:29 +0800

    feat: 增加 AdminAlert 组件

---
+++ src/blazor/admin/BootstrapAdmin.Web/Components/AdminAlert.razor.cs
@@ -0,0 +1,25 @@
+﻿using BootstrapAdmin.Web.Core;
+
+namespace BootstrapAdmin.Web.Components;
+
+public partial class AdminAlert
+{
+    [Parameter]
+    public string? Text { get; set; }
+
+    private bool IsDemo { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IDict? DictService { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        IsDemo = DictService.IsDemo();
+    }
+}
