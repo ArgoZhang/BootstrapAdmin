c15de686c0debafe20b4aa9f63c2867379eed05d
Argo-Tianyi <argo@163.com>
2021-12-25 12:37:05 +0800

    feat: 增加 Profiles 页面

--- src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Profiles.razor.cs
+++ src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Profiles.razor.cs
@@ -1,6 +1,38 @@
-﻿namespace BootstrapAdmin.Web.Pages.Admin
+﻿using BootstrapAdmin.DataAccess.Models;
+using BootstrapAdmin.Web.Core;
+using BootstrapAdmin.Web.Services;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BootstrapAdmin.Web.Pages.Admin;
+
+public partial class Profiles
 {
-    public partial class Profiles
+    public bool IsDemo { get; set; }
+
+    [NotNull]
+    private User? CurrentUser { get; set; }
+
+    [Inject]
+    [NotNull]
+    public BootstrapAppContext? AppContext { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void OnInitialized()
     {
+        base.OnInitialized();
+
+        CurrentUser = new User()
+        {
+            App = AppContext.AppId,
+            UserName = AppContext.UserName,
+            DisplayName = AppContext.DisplayName
+        };
+    }
+
+    private Task OnSaveDisplayName(EditContext context)
+    {
+        return Task.CompletedTask;
     }
 }
