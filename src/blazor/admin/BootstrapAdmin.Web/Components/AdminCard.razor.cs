874b45b29ba9c97ad9f374675d64dbace148c945
Argo-Tianyi <argo@163.com>
2021-12-25 13:02:54 +0800

    feat: 增加 AdminCard 组件

---
+++ src/blazor/admin/BootstrapAdmin.Web/Components/AdminCard.razor.cs
@@ -0,0 +1,28 @@
+﻿namespace BootstrapAdmin.Web.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class AdminCard
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? AuthorizeKey { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        [EditorRequired]
+        [NotNull]
+        public string? HeaderText { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+    }
+}
