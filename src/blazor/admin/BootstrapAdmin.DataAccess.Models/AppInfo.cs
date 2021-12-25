083bd15d91aa88bdda5c09a54cee3ed8c614bc75
Argo-Tianyi <argo@163.com>
2021-12-25 20:44:34 +0800

    feat: 增加网站设置

---
+++ src/blazor/admin/BootstrapAdmin.DataAccess.Models/AppInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BootstrapAdmin.DataAccess.Models;
+
+public class AppInfo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Display(Name = "系统名称")]
+    [Required(ErrorMessage = "{0}不可为空")]
+    public string? Title { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Display(Name = "网站页脚")]
+    [Required(ErrorMessage = "{0}不可为空")]
+    public string? Footer { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Display(Name = "登录地址")]
+    public string? Login { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Display(Name = "后台地址")]
+    public string? AuthUrl { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Theme { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool IsDemo { get; set; }
+}
--- src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Settings.razor.cs
+++ src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Settings.razor.cs
@@ -1,6 +1,72 @@
-﻿namespace BootstrapAdmin.Web.Pages.Admin
+﻿using BootstrapAdmin.DataAccess.Models;
+using BootstrapAdmin.Web.Core;
+using BootstrapAdmin.Web.Extensions;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BootstrapAdmin.Web.Pages.Admin
 {
     public partial class Settings
     {
+        private bool IsDemo { get; set; }
+
+        [NotNull]
+        private AppInfo? AppInfo { get; set; }
+
+        [NotNull]
+        private List<SelectedItem>? Logins { get; set; }
+
+        [NotNull]
+        private List<SelectedItem>? Themes { get; set; }
+
+        [Inject]
+        [NotNull]
+        private IDict? DictService { get; set; }
+
+        [Inject]
+        [NotNull]
+        private IUser? UserService { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            IsDemo = DictService.IsDemo();
+            Logins = DictService.GetLogins().ToSelectedItemList();
+            Themes = DictService.GetThemes().ToSelectedItemList();
+            AppInfo = new();
+        }
+
+        private Task OnSaveTitle(EditContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task OnSaveFooter(EditContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task OnSaveLogin(EditContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task OnSaveAuthUrl(EditContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        private Task OnSaveTheme(EditContext context)
+        {
+            return Task.CompletedTask;
+        }
+
+        private void OnSaveDemo()
+        {
+            IsDemo = AppInfo.IsDemo;
+        }
     }
 }
