e1a6e7379d39baa4d86acc4aaecde7fb43a8852d
Argo-Tianyi <argo@163.com>
2022-01-06 06:45:10 +0800

    feat: 拆分登录组件

--- src/blazor/admin/BootstrapAdmin.Web/Pages/Account/Login.razor.cs
+++ src/blazor/admin/BootstrapAdmin.Web/Pages/Account/Login.razor.cs
@@ -1,99 +1,11 @@
-﻿using BootstrapAdmin.Web.Core;
-using Microsoft.JSInterop;
-
-namespace BootstrapAdmin.Web.Pages.Account;
+﻿namespace BootstrapAdmin.Web.Pages.Account;
 
 /// <summary>
 /// 
 /// </summary>
 public partial class Login
 {
-    private string? Title { get; set; }
-
-    private bool AllowMobile { get; set; } = true;
-
-    private bool UseMobileLogin { get; set; }
-
-    private bool AllowOAuth { get; set; } = true;
-
-    private bool RememberPassword { get; set; }
-
-    private ElementReference LoginForm { get; set; }
-
-    private string? PostUrl { get; set; }
-
     [SupplyParameterFromQuery]
     [Parameter]
     public string? ReturnUrl { get; set; }
-
-    [Inject]
-    [NotNull]
-    private IDict? DictsService { get; set; }
-
-    [Inject]
-    [NotNull]
-    private IJSRuntime? JSRuntime { get; set; }
-
-    private string? UserName { get; set; }
-
-    private string? Password { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    protected override void OnInitialized()
-    {
-        base.OnInitialized();
-
-        Title = DictsService.GetWebTitle();
-
-        PostUrl = QueryHelper.AddQueryString("/Account/Login", "ReturnUrl", ReturnUrl ?? "");
-
-#if DEBUG
-        UserName = "Admin";
-        Password = "123789";
-#endif
-    }
-
-    /// <summary>
-    /// OnAfterRenderAsync 方法
-    /// </summary>
-    /// <param name="firstRender"></param>
-    /// <returns></returns>
-    protected override async Task OnAfterRenderAsync(bool firstRender)
-    {
-        await base.OnAfterRenderAsync(firstRender);
-
-        if (firstRender)
-        {
-            // register javascript
-            await JSRuntime.InvokeVoidAsync("$.login", LoginForm, "api/Login");
-        }
-    }
-
-    void OnClickSwitchButton()
-    {
-        var rem = RememberPassword ? "true" : "false";
-        PostUrl = QueryHelper.AddQueryString(UseMobileLogin ? "/Account/Mobile" : "/Account/Login", new Dictionary<string, string?>()
-        {
-            [nameof(ReturnUrl)] = ReturnUrl,
-            ["remember"] = rem
-        });
-    }
-
-    Task OnRememberPassword(bool remember)
-    {
-        OnClickSwitchButton();
-        return Task.CompletedTask;
-    }
-
-    void OnSignUp()
-    {
-
-    }
-
-    void OnForgotPassword()
-    {
-
-    }
 }
