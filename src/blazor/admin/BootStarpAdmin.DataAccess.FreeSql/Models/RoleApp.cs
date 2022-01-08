27ff89ee6479c479a21f829d5537443f40281283
zhangpeihang <948869991@qq.com>
2022-01-08 11:50:53 +0800

    feat: 添加 App 服务

---
+++ src/blazor/admin/BootStarpAdmin.DataAccess.FreeSql/Models/RoleApp.cs
@@ -0,0 +1,8 @@
+﻿namespace BootStarpAdmin.DataAccess.FreeSql.Models;
+
+public class RoleApp
+{
+    public string? RoleID { get; set; }
+
+    public string? AppID { get; set; }
+}
---
+++ src/blazor/admin/BootStarpAdmin.DataAccess.FreeSql/Service/AppService.cs
@@ -0,0 +1,32 @@
+﻿using BootStarpAdmin.DataAccess.FreeSql.Models;
+using BootstrapAdmin.Web.Core;
+
+namespace BootStarpAdmin.DataAccess.FreeSql.Service;
+
+public class AppService : IApp
+{
+    private IFreeSql FreeSql;
+
+    public AppService(IFreeSql freeSql) => FreeSql = freeSql;
+
+    public List<string> GetAppsByRoleId(string? roleId) => FreeSql.Ado.Query<string>("select AppID from RoleApp where RoleID = @roleId", new { roleId = roleId });
+
+    public bool SaveAppsByRoleId(string? roleId, IEnumerable<string> appIds)
+    {
+        var ret = false;
+        try
+        {
+            FreeSql.Transaction(() =>
+            {
+                FreeSql.Ado.ExecuteNonQuery("delete from RoleApp where RoleID = @roleId", new { roleId = roleId });
+                FreeSql.Insert(appIds.Select(g => new RoleApp { AppID = g, RoleID = roleId }));
+                ret = true;
+            });
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        return ret;
+    }
+}
