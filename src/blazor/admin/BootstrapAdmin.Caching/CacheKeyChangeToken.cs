61ac109d2e52f1cb422b5f4e9c0c75724ab946a0
Argo-Tianyi <argo@163.com>
2022-01-14 21:34:52 +0800

    feat: 准备实现 IChangeToken 类

---
+++ src/blazor/admin/BootstrapAdmin.Caching/CacheKeyChangeToken.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace BootstrapAdmin.Caching;
+
+/// <summary>
+/// 缓存键值 IChangeToken 实现类
+/// </summary>
+public class CacheKeyChangeToken : IChangeToken
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool HasChanged { get; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public bool ActiveChangeCallbacks { get; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="callback"></param>
+    /// <param name="state"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public IDisposable RegisterChangeCallback(Action<object> callback, object state)
+    {
+        throw new NotImplementedException();
+    }
+}
