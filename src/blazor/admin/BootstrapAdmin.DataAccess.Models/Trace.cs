2969e1749e619f65278147da732c874a5a686129
Argo-Tianyi <argo@163.com>
2022-01-17 12:33:46 +0800

    feat: 增加访问日志

---
+++ src/blazor/admin/BootstrapAdmin.DataAccess.Models/Trace.cs
@@ -0,0 +1,57 @@
+﻿namespace BootstrapAdmin.DataAccess.Models;
+
+/// <summary>
+/// 
+/// </summary>
+public class Trace
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Id { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? UserName { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public DateTime LogTime { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Ip { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Browser { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? OS { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? City { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? RequestUrl { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? UserAgent { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? Referer { get; set; }
+}
---
+++ src/blazor/admin/BootstrapAdmin.Web.Core/ITrace.cs
@@ -0,0 +1,15 @@
+﻿using BootstrapAdmin.DataAccess.Models;
+
+namespace BootstrapAdmin.Web.Core;
+
+/// <summary>
+/// 
+/// </summary>
+public interface ITrace
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="trace"></param>
+    public void Log(Trace trace);
+}
