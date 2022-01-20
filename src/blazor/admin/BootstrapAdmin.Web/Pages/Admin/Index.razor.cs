36aa19dd6e3144fa31c497c9c7f78966cc9ceb12
Argo-Tianyi <argo@163.com>
2022-01-20 10:52:11 +0800

    feat: 增加登录帮助类用于获取默认导航页面

---
+++ src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Index.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace BootstrapAdmin.Web.Pages.Admin;
+
+/// <summary>
+/// 
+/// </summary>
+[AllowAnonymous]
+public partial class Index
+{
+
+}
---
+++ src/blazor/admin/BootstrapAdmin.Web/Utils/LoginHelper.cs
@@ -0,0 +1,41 @@
+﻿using BootstrapAdmin.Web.Core;
+
+namespace BootstrapAdmin.Web.Utils;
+
+/// <summary>
+/// 登录获取默认首页帮助类
+/// </summary>
+public static class LoginHelper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="userName"></param>
+    /// <param name="returnUrl"></param>
+    /// <param name="appId"></param>
+    /// <param name="defaultAppId"></param>
+    /// <param name="userService"></param>
+    /// <param name="dictService"></param>
+    /// <returns></returns>
+    public static string GetDefaultUrl(string userName, string? returnUrl, string? appId, string defaultAppId, IUser userService, IDict dictService)
+    {
+        if (string.IsNullOrEmpty(returnUrl))
+        {
+            // 查找 User 设置的默认应用
+            appId ??= userService.GetAppIdByUserName(userName) ?? defaultAppId;
+
+            if (appId == defaultAppId && dictService.GetEnableDefaultApp())
+            {
+                // 开启默认应用
+                appId = dictService.GetApps().FirstOrDefault(d => d.Key != defaultAppId).Key;
+            }
+
+            if (!string.IsNullOrEmpty(appId))
+            {
+                returnUrl = dictService.GetHomeUrlByAppId(appId);
+            }
+        }
+
+        return returnUrl ?? "/Admin/Index";
+    }
+}
