0d609c41b0c89359b4ba59d4ab25ff459c48b3fe
Argo-Tianyi <argo@163.com>
2022-01-21 12:30:16 +0800

    feat: 登录日志增加默认 LoginTime 倒序排序功能

--- src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Logins.razor.cs
+++ src/blazor/admin/BootstrapAdmin.Web/Pages/Admin/Logins.razor.cs
@@ -11,4 +11,6 @@ public partial class Logins
     /// 
     /// </summary>
     public ITableSearchModel TableSearchModel { get; set; } = new LoginLogModel();
+
+    private List<string> SortList { get; set; } = new List<string>() { "LoginTime desc" };
 }
