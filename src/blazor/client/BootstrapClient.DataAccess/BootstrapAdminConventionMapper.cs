b4aea5974cf5fe7ef48ca330a477449f6743fb99
Argo-Tianyi <argo@163.com>
2022-01-24 00:47:19 +0800

    chore: 更新依赖

--- src/blazor/client/BootstrapClient.DataAccess/BootstrapAdminConventionMapper.cs
+++ src/blazor/client/BootstrapClient.DataAccess/BootstrapAdminConventionMapper.cs
@@ -20,54 +20,16 @@ class BootstrapAdminConventionMapper : ConventionMapper
 
         ti.TableName = pocoType.Name switch
         {
-            "Error" => "Exceptions",
             _ => $"{pocoType.Name}s"
         };
         return ti;
     }
 
-    ///// <summary>
-    ///// 
-    ///// </summary>
-    ///// <param name="pocoProperty"></param>
-    ///// <returns></returns>
-    //public override ColumnInfo GetColumnInfo(PropertyInfo pocoProperty) => pocoProperty.DeclaringType?.Name switch
-    //{
-    //    nameof(Models.User) => GetUserColumnInfo(pocoProperty),
-    //    nameof(Models.Navigation) => GetNavigationColumnInfo(pocoProperty),
-    //    _ => base.GetColumnInfo(pocoProperty)
-    //};
+    public override Func<object?, object?> GetFromDbConverter(PropertyInfo targetProperty, Type sourceType) => targetProperty.PropertyType.IsEnum && sourceType == typeof(string)
+        ? new NumberToEnumConverter(targetProperty.PropertyType).ConvertFromDb
+        : base.GetFromDbConverter(targetProperty, sourceType);
 
-    //private ColumnInfo GetUserColumnInfo(PropertyInfo pocoProperty)
-    //{
-    //    var ci = base.GetColumnInfo(pocoProperty);
-    //    var resultColumns = new List<string>
-    //    {
-    //        nameof(Models.User.NewPassword),
-    //        nameof(Models.User.ConfirmPassword),
-    //        nameof(Models.User.Period),
-    //        nameof(Models.User.IsReset)
-    //    };
-    //    ci.ResultColumn = resultColumns.Any(c => c == ci.ColumnName);
-    //    return ci;
-    //}
-
-    //private ColumnInfo GetNavigationColumnInfo(PropertyInfo pocoProperty)
-    //{
-    //    var ci = base.GetColumnInfo(pocoProperty);
-    //    var resultColumns = new List<string>
-    //    {
-    //        nameof(Models.Navigation.HasChildren)
-    //    };
-    //    ci.ResultColumn = resultColumns.Any(c => c == ci.ColumnName);
-    //    return ci;
-    //}
-
-    //public override Func<object?, object?> GetFromDbConverter(PropertyInfo targetProperty, Type sourceType) => targetProperty.PropertyType.IsEnum && sourceType == typeof(string)
-    //    ? new StringToEnumConverter(targetProperty.PropertyType).ConvertFromDb
-    //    : base.GetFromDbConverter(targetProperty, sourceType);
-
-    //public override Func<object?, object?> GetToDbConverter(PropertyInfo targetProperty) => targetProperty.PropertyType.IsEnum
-    //    ? new StringToEnumConverter(targetProperty.PropertyType).ConvertToDb
-    //    : base.GetToDbConverter(targetProperty);
+    public override Func<object?, object?> GetToDbConverter(PropertyInfo targetProperty) => targetProperty.PropertyType.IsEnum
+        ? new NumberToEnumConverter(targetProperty.PropertyType).ConvertToDb
+        : base.GetToDbConverter(targetProperty);
 }
