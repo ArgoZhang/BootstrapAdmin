fa7fae95754481967851520c575b33f862bca1af
zhangpeihang <948869991@qq.com>
2022-01-27 17:33:50 +0800

    feat: 添加 字典 菜单 角色 用户 控制器

---
+++ src/vue/api/BootstrapAdmin.Api/Controllers/DictsController.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+using BootstrapAdmin.DataAccess.Models;
+using BootstrapAdmin.Web.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapAdmin.Api.Controllers
+{
+    /// <summary>
+    /// 字典表维护控制器
+    /// </summary>
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class DictsController : ControllerBase
+    {
+        private IDict DictService { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dictService"></param>
+        public DictsController(IDict dictService) => DictService = dictService;
+
+        /// <summary>
+        /// 获取所有字典表数据方法
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<Dict>> Get()
+        {
+            return DictService.GetAll();
+        }
+        /// <summary>
+        /// 保存字典方法
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public bool Post([FromBody] Dict value)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 删除字典项方法
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpDelete]
+        [Authorize(Roles = "Administrators")]
+        public bool Delete([FromBody] IEnumerable<string> value)
+        {
+            return true;
+        }
+    }
+}
---
+++ src/vue/api/BootstrapAdmin.Api/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+using BootstrapAdmin.DataAccess.Models;
+using BootstrapAdmin.Web.Core;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapAdmin.Api.Controllers
+{
+    /// <summary>
+    /// 用户控制器
+    /// </summary>
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUser UserService { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userService"></param>
+        public UsersController(IUser userService) => UserService = userService;
+
+        /// <summary>
+        /// 调用获取所有用户信息 用户管理查询按钮
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<User>> Get()
+        {
+            return UserService.GetAll();
+        }
+
+        /// <summary>
+        /// api 握手协议
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpOptions]
+        public string? Options()
+        {
+            return null;
+        }
+    }
+}
