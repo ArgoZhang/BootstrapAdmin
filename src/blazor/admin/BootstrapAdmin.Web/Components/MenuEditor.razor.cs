65d9d4563a3ec747d1a34780fb595ab1ffb19b40
Argo-Tianyi <argo@163.com>
2022-02-28 19:37:27 +0800

    feat: 完善菜单维护父级菜单功能

---
+++ src/blazor/admin/BootstrapAdmin.Web/Components/MenuEditor.razor.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+using BootstrapAdmin.DataAccess.Models;
+
+namespace BootstrapAdmin.Web.Components;
+
+public partial class MenuEditor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    [NotNull]
+    public Navigation? Value { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    [NotNull]
+    public List<SelectedItem>? ParementMenus { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    [NotNull]
+    public List<SelectedItem>? Targets { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    [NotNull]
+    public List<SelectedItem>? Apps { get; set; }
+}
---
+++ src/blazor/admin/BootstrapAdmin.Web/Components/ParentMenuTree.razor.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+using BootstrapAdmin.Web.Core;
+using BootstrapAdmin.Web.Extensions;
+using BootstrapAdmin.Web.Services;
+
+namespace BootstrapAdmin.Web.Components;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class ParentMenuTree
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    [NotNull]
+    public string? Value { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public EventCallback<string> ValueChanged { get; set; }
+
+    [NotNull]
+    private List<TreeItem>? InternalItems { get; set; }
+
+    [Inject]
+    [NotNull]
+    private INavigation? NavigationService { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IDict? DictService { get; set; }
+
+    [Inject]
+    [NotNull]
+    private BootstrapAppContext? Context { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        var items = NavigationService.GetAllMenus(Context.UserName);
+        InternalItems = items.ToTreeItemList(new List<string> { Value }, RenderTreeItem);
+    }
+
+    private async Task OnTreeItemChecked(List<TreeItem> items)
+    {
+        Value = items.First().Key?.ToString();
+        if (ValueChanged.HasDelegate)
+        {
+            await ValueChanged.InvokeAsync(Value);
+        }
+    }
+
+    private string GetApp(string? app) => DictService.GetApps().FirstOrDefault(i => i.Key == app).Value ?? "未设置";
+}
