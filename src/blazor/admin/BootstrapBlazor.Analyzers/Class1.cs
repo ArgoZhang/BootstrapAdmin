3f80b88d72dc8d2f035a778a6cb1e58e4952553a
Argo-Tianyi <argo@163.com>
2022-02-28 19:38:49 +0800

    wip: 增加分析器工程

---
+++ src/blazor/admin/BootstrapBlazor.Analyzers/Class1.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace BootstrapBlazor.Analyzers;
+
+/// <summary>
+/// 
+/// </summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class BlazorAnalyzer : DiagnosticAnalyzer
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(new DiagnosticDescriptor("Test", "Test", "{0} Test", "Naming", DiagnosticSeverity.Warning, isEnabledByDefault: true, description: "Test")); } }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="context"></param>
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+        context.RegisterSyntaxNodeAction(syntaxNodeContext =>
+        {
+
+        }, SyntaxKind.IdentifierName);
+    }
+}
