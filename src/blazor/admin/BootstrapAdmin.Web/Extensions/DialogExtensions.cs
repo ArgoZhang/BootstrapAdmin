89b9bf7e3a9c983c6d9e1f7503742e5b39058873
Argo <argo@163.com>
2022-04-02 12:49:54 +0000

    !102 perf(#I50WR9): 优化 NavigationTree 逻辑提高性能 * refactor: 增加内置滚动条样式 * refactor: 优化 NavigationTree 性能

--- src/blazor/admin/BootstrapAdmin.Web/Extensions/DialogExtensions.cs
+++ src/blazor/admin/BootstrapAdmin.Web/Extensions/DialogExtensions.cs
@@ -18,9 +18,48 @@ public static class DialogExtensions
     public static Task ShowAssignmentDialog(this DialogService dialogService, string title, List<SelectedItem> items, List<string> value, Func<Task<bool>> saveCallback, ToastService? toast) => dialogService.ShowDialog<Assignment, SelectedItem>(title, items, value, saveCallback, toast);
 
     /// <summary>
-    /// 
+    /// 弹出菜单分配弹窗
     /// </summary>
-    public static Task ShowNavigationDialog(this DialogService dialogService, string title, List<Navigation> items, List<string> value, Func<Task<bool>> saveCallback, ToastService? toast) => dialogService.ShowDialog<NavigationTree, Navigation>(title, items, value, saveCallback, toast);
+    /// <param name="dialogService"></param>
+    /// <param name="title">弹窗标题</param>
+    /// <param name="menus">当前用户可用所有菜单集合</param>
+    /// <param name="value">已分配菜单集合</param>
+    /// <param name="saveCallback"></param>
+    /// <param name="toast"></param>
+    /// <returns></returns>
+    public static async Task ShowNavigationDialog(this DialogService dialogService, string title, List<Navigation> menus, List<string> value, Func<List<string>, Task<bool>> saveCallback, ToastService? toast)
+    {
+        var option = new DialogOption
+        {
+            Title = title,
+            ShowFooter = false,
+            IsScrolling = true,
+            Class = "modal-dialog-menu"
+        };
+        var parameters = new Dictionary<string, object?>()
+        {
+            [nameof(NavigationTree.Items)] = menus,
+            [nameof(NavigationTree.Value)] = value,
+            [nameof(NavigationTree.OnClose)] = () => option.Dialog.Close(),
+            [nameof(NavigationTree.OnSave)] = new Func<List<string>, Task>(async items =>
+            {
+                var ret = await saveCallback(items);
+                if (toast != null)
+                {
+                    if (ret)
+                    {
+                        await toast.Success("分配操作", "操作成功！");
+                    }
+                    else
+                    {
+                        await toast.Error("分配操作", "操作失败，请联系相关管理员！");
+                    }
+                }
+            })
+        };
+        option.Component = BootstrapDynamicComponent.CreateComponent<NavigationTree>(parameters);
+        await dialogService.Show(option);
+    }
 
     private static Task ShowDialog<TBody, TItem>(this DialogService dialogService, string title, List<TItem> items, List<string> value, Func<Task<bool>> saveCallback, ToastService? toast) where TBody : AssignmentBase<TItem> => dialogService.ShowSaveDialog<TBody>(title, async () =>
     {
