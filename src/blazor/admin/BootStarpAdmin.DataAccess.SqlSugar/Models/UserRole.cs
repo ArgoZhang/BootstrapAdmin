21253a1ea0d306c020e9161660774a2129c31680
zhangpeihang <948869991@qq.com>
2022-04-26 11:17:17 +0800

    feat: 添加中间表

---
+++ src/blazor/admin/BootStarpAdmin.DataAccess.SqlSugar/Models/UserRole.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootStarpAdmin.DataAccess.SqlSugar.Models;
+
+/// <summary>
+/// 
+/// </summary>
+public class UserRole
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? RoleId { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public string? UserId { get; set; }
+}
