203b3fd37f5a4f78ef170414b4b7945c123c417e
zhangpeihang <948869991@qq.com>
2022-04-29 15:07:16 +0800

    feat: 添加中间表

---
+++ src/blazor/admin/BootStarpAdmin.DataAccess.SqlSugar/Models/RoleGroup.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+namespace BootStarpAdmin.DataAccess.SqlSugar.Models;
+
+/// <summary>
+/// 
+/// </summary>
+public class RoleGroup
+{
+    public string? ID { get; set; }
+
+    public string? RoleID { get; set; }
+
+    public string? GroupID { get; set; }
+}
