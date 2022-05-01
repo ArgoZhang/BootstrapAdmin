a7aca2256120f7fdf6e18dcf795ddf2dd8960674
zhangpeihang <948869991@qq.com>
2022-05-01 11:54:56 +0800

    feat: 完善 APP 服务

--- src/blazor/admin/BootStarpAdmin.DataAccess.FreeSql/Service/AppService.cs
+++ src/blazor/admin/BootStarpAdmin.DataAccess.FreeSql/Service/AppService.cs
@@ -3,17 +3,20 @@
 // Website: https://admin.blazor.zone
 
 using BootStarpAdmin.DataAccess.FreeSql.Models;
+using BootstrapAdmin.Caching;
 using BootstrapAdmin.Web.Core;
 
 namespace BootStarpAdmin.DataAccess.FreeSql.Service;
 
 class AppService : IApp
 {
+    private const string AppServiceGetAppsByRoleIdCacheKey = "AppService-GetAppsByRoleId";
+
     private IFreeSql FreeSql { get; }
 
     public AppService(IFreeSql freeSql) => FreeSql = freeSql;
 
-    public List<string> GetAppsByRoleId(string? roleId) => FreeSql.Ado.Query<string>("select AppID from RoleApp where RoleID = @roleId", new { roleId });
+    public List<string> GetAppsByRoleId(string? roleId) => CacheManager.GetOrAdd($"{AppServiceGetAppsByRoleIdCacheKey}-{roleId}", entry => FreeSql.Ado.Query<string>("select AppID from RoleApp where RoleID = @roleId", new { roleId }));
 
     public bool SaveAppsByRoleId(string? roleId, IEnumerable<string> appIds)
     {
@@ -30,6 +33,10 @@ class AppService : IApp
         catch (Exception)
         {
             throw;
+        }
+        if (ret)
+        {
+            CacheManager.Clear();
         }
         return ret;
     }
