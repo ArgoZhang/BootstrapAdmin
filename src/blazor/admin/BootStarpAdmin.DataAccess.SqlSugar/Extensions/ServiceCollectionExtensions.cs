294a9a9c6b18d30eaa090f20d54e393331806455
Argo-AscioTech <argo@live.ca>
2024-04-25 15:36:02 +0800

    refactor: 更新 SqlSugar ORM 扩展方法

--- src/blazor/admin/BootStarpAdmin.DataAccess.SqlSugar/Extensions/ServiceCollectionExtensions.cs
+++ src/blazor/admin/BootStarpAdmin.DataAccess.SqlSugar/Extensions/ServiceCollectionExtensions.cs
@@ -2,8 +2,7 @@
 // Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
 // Website: https://admin.blazor.zone
 
-
-using BootstrapAdmin.DataAccess.Models;
+using BootstrapAdmin.DataAccess.SqlSugar.Extensions;
 using BootstrapAdmin.DataAccess.SqlSugar.Service;
 using BootstrapAdmin.Web.Core;
 using BootstrapBlazor.Components;
@@ -21,96 +20,29 @@ public static class ServiceCollectionExtensions
     /// 注入 SqlSugar 数据服务类
     /// </summary>
     /// <param name="services"></param>
-    /// <param name="dbType">数据库类型</param>
-    /// <param name="dbName"></param>
-    /// <param name="dbProvider"></param>
-    /// <returns></returns>
-    public static IServiceCollection AddSqlSugar(this IServiceCollection services, DbType dbType, string dbName, Action<IServiceProvider, SqlSugarClient>? dbProvider = null)
+    public static IServiceCollection AddSqlSugarDataAccessServices(this IServiceCollection services)
     {
         // 增加缓存服务
         services.AddCacheManager();
+
         services.TryAddSingleton<ISqlSugarClient>(provider =>
         {
             StaticConfig.Check_StringIdentity = false;
             var configuration = provider.GetRequiredService<IConfiguration>();
-            var connString = configuration.GetConnectionString(dbName);
+            var connString = configuration.GetConnectionString("ba");
             return new SqlSugarClient(new ConnectionConfig()
             {
                 ConnectionString = connString,
-                DbType = dbType,
+                DbType = DbType.Sqlite,
                 IsAutoCloseConnection = true,
                 InitKeyType = InitKeyType.Attribute,
+                ConfigureExternalServices = SqlSugarHelper.InitConfigureExternalServices()
 
-                ConfigureExternalServices = new ConfigureExternalServices()
-                {
-                    EntityNameService = (type, entity) =>
-                    {
-                        if (entity.DbTableName == nameof(User))
-                        {
-                            entity.DbTableName = "Users";
-                        }
-                        else if (entity.DbTableName == nameof(Dict))
-                        {
-                            entity.DbTableName = "Dicts";
-                        }
-                        else if (entity.DbTableName == nameof(Navigation))
-                        {
-                            entity.DbTableName = "Navigations";
-                        }
-                        else if (entity.DbTableName == nameof(Group))
-                        {
-                            entity.DbTableName = "Groups";
-                        }
-                        else if (entity.DbTableName == nameof(Role))
-                        {
-                            entity.DbTableName = "Roles";
-                        }
-                        else if (entity.DbTableName == nameof(Error))
-                        {
-                            entity.DbTableName = "Errors";
-                        }
-                        else if (entity.DbTableName == nameof(Trace))
-                        {
-                            entity.DbTableName = "Traces";
-                        }
-                        else if (entity.DbTableName == nameof(LoginLog))
-                        {
-                            entity.DbTableName = "LoginLogs";
-                        }
-                    },
-                    EntityService = (type, column) =>
-                    {
-                        if (column.DbTableName == "Users")
-                        {
-                            if (column.DbColumnName == nameof(User.Period)
-                            || column.DbColumnName == nameof(User.ConfirmPassword)
-                            || column.DbColumnName == nameof(User.NewPassword)
-                            || column.DbColumnName == nameof(User.IsReset)
-                            )
-                            {
-                                column.IsIgnore = true;
-                            }
-                        }
-                        else if (column.DbTableName == "Navigations")
-                        {
-                            if (column.DbColumnName == nameof(Navigation.HasChildren))
-                            {
-                                column.IsIgnore = true;
-                            }
-                        }
-                        if (column.DbColumnName.ToUpper() == "ID")
-                        {
-                            column.IsPrimarykey = true;
-                            column.IsIdentity = true;
-                        }
-                    }
-                }
             }, db =>
             {
-                dbProvider?.Invoke(provider, db);
                 db.Aop.OnLogExecuting = (sql, pars) =>
                 {
-                    System.Console.WriteLine(sql);//输出sql
+                    System.Console.WriteLine(sql);
                 };
             });
         });
---
+++ src/blazor/admin/BootStarpAdmin.DataAccess.SqlSugar/Extensions/SqlSugarHelper.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://admin.blazor.zone
+
+using BootstrapAdmin.DataAccess.Models;
+
+namespace BootstrapAdmin.DataAccess.SqlSugar.Extensions;
+
+static class SqlSugarHelper
+{
+    public static ConfigureExternalServices InitConfigureExternalServices()
+    {
+        return new ConfigureExternalServices()
+        {
+            EntityNameService = (type, entity) =>
+            {
+                if (entity.DbTableName == nameof(User))
+                {
+                    entity.DbTableName = "Users";
+                }
+                else if (entity.DbTableName == nameof(Dict))
+                {
+                    entity.DbTableName = "Dicts";
+                }
+                else if (entity.DbTableName == nameof(Navigation))
+                {
+                    entity.DbTableName = "Navigations";
+                }
+                else if (entity.DbTableName == nameof(Group))
+                {
+                    entity.DbTableName = "Groups";
+                }
+                else if (entity.DbTableName == nameof(Role))
+                {
+                    entity.DbTableName = "Roles";
+                }
+                else if (entity.DbTableName == nameof(Error))
+                {
+                    entity.DbTableName = "Errors";
+                }
+                else if (entity.DbTableName == nameof(Trace))
+                {
+                    entity.DbTableName = "Traces";
+                }
+                else if (entity.DbTableName == nameof(LoginLog))
+                {
+                    entity.DbTableName = "LoginLogs";
+                }
+            },
+            EntityService = (type, column) =>
+            {
+                if (column.DbTableName == "Users")
+                {
+                    if (column.DbColumnName == nameof(User.Period)
+                    || column.DbColumnName == nameof(User.ConfirmPassword)
+                    || column.DbColumnName == nameof(User.NewPassword)
+                    || column.DbColumnName == nameof(User.IsReset)
+                    )
+                    {
+                        column.IsIgnore = true;
+                    }
+                }
+                else if (column.DbTableName == "Navigations")
+                {
+                    if (column.DbColumnName == nameof(Navigation.HasChildren))
+                    {
+                        column.IsIgnore = true;
+                    }
+                }
+                if (column.DbColumnName.ToUpper() == "ID")
+                {
+                    column.IsPrimarykey = true;
+                    column.IsIdentity = true;
+                }
+            }
+        };
+    }
+}
