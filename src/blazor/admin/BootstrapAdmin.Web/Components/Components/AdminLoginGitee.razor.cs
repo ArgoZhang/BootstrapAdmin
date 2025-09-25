c4f6e6de09b10a6d64fcdce25e2c8ad36c63689f
Argo Zhang <argo@live.ca>
2025-09-25 11:13:17 +0800

    feat: add client validate function on login (#24)
    * refactor: 更新路径
    
    * feat: 增加脚本
    
    * feat: 增加客户端验证

---
+++ src/blazor/admin/BootstrapAdmin.Web/Components/Components/AdminLoginGitee.razor.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://pro.blazor.zone
+
+using BootstrapAdmin.Web.Core;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BootstrapAdmin.Web.Components.Components;
+
+[JSModuleAutoLoader("./Components/Components/AdminLoginGitee.razor.js", JSObjectReference = true)]
+public partial class AdminLoginGitee
+{
+    private string? _userName;
+
+    private string? _password;
+
+    [Inject, NotNull]
+    private ToastService? ToastService { get; set; }
+
+    [Inject, NotNull]
+    private IUser? UserService { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task<bool> TriggerLogin()
+    {
+        var ret = false;
+        if (!string.IsNullOrEmpty(_userName) && !string.IsNullOrEmpty(_password))
+        {
+            ret = UserService.Authenticate(_userName, _password);
+        }
+        if (!ret)
+        {
+            await ToastService.Error("登录", "用户名或密码错误");
+        }
+        return ret;
+    }
+}
