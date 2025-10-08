166af262832bff911617fa72db5407213b78f3db
Argo Zhang <argo@live.ca>
2025-10-08 10:11:46 +0800

    feat: 增加 AccessDenied 控制器

---
+++ src/blazor/admin/BootstrapAdmin.Web/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
+// Website: https://pro.blazor.zone
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapAdmin.Web.Controllers;
+
+/// <summary>
+/// 
+/// </summary>
+public class AccountController : Controller
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult AccessDenied()
+    {
+        return Ok();
+    }
+}
