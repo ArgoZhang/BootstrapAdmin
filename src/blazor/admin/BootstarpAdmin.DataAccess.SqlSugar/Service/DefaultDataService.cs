54994e2f6f0c3c6849f8834422bb423e24abbda2
Argo Zhang <argo@live.ca>
2025-10-31 11:09:21 +0800

    doc: update documentation email (#30)
    * chore: 更新邮箱
    
    * chore: 更新邮件信箱
    
    * refactor: 更新依赖
    
    * chore: 更新 footer 配置项
    
    * chore: 使用新项目格式

--- src/blazor/admin/BootstarpAdmin.DataAccess.SqlSugar/Service/DefaultDataService.cs
+++ src/blazor/admin/BootstarpAdmin.DataAccess.SqlSugar/Service/DefaultDataService.cs
@@ -1,6 +1,7 @@
-﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
 // Licensed under the LGPL License, Version 3.0. See License.txt in the project root for license information.
-// Website: https://admin.blazor.zone
+// Website: https://pro.blazor.zone
+
 using BootstrapBlazor.Components;
 
 namespace BootstrapAdmin.DataAccess.SqlSugar.Service;
